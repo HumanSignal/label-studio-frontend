ed0d8ef13e54b22be673cc0071c1ad4887f88969
Mikhail Maluyk <mikhail.maluyk@gmail.com>
2020-02-24 06:52:00 +0300

    static build

---
+++ build/precache-manifest.558047a076bb99cc2c0aae0816e16933.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "695e150122c29914f4bb",
+    "url": "/static/css/main.d9f16a7a.css"
+  },
+  {
+    "revision": "695e150122c29914f4bb",
+    "url": "/static/js/main.695e1501.js"
+  },
+  {
+    "revision": "148c5406e727575227630ab996225299",
+    "url": "/index.html"
+  }
+];
