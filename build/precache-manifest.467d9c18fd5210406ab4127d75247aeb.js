59ba2823bc841e820c80df25c9d11146a91e18a8
Mikhail Maluyk <mikhail.maluyk@gmail.com>
2020-03-05 11:06:25 +0300

    small fix for highlighting the region

---
+++ build/precache-manifest.467d9c18fd5210406ab4127d75247aeb.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "409dc03f34086425648e",
+    "url": "/static/css/main.ce7363e6.css"
+  },
+  {
+    "revision": "409dc03f34086425648e",
+    "url": "/static/js/main.409dc03f.js"
+  },
+  {
+    "revision": "124bc7f3962c758e0befc9a1cee7545f",
+    "url": "/index.html"
+  }
+];
