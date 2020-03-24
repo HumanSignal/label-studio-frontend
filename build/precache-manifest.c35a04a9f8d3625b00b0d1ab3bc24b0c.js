fff261f6ca08766317872aaecb43c14fe9a1d4be
Michael Malyuk <mikhail.maluyk@gmail.com>
2020-03-24 18:38:41 +0300

    fixing a typo (#39)

---
+++ build/precache-manifest.c35a04a9f8d3625b00b0d1ab3bc24b0c.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1677317362285956a0a",
+    "url": "/static/css/main.65efca7b.css"
+  },
+  {
+    "revision": "f1677317362285956a0a",
+    "url": "/static/js/main.f1677317.js"
+  },
+  {
+    "revision": "4dcf0b0b72349240fb359429c4ca16c9",
+    "url": "/index.html"
+  }
+];
