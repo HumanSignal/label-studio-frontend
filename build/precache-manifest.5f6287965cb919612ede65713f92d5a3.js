10e59463f665cfed1abb7301125835b551b83a41
Mikhail Maluyk <mikhail.maluyk@gmail.com>
2020-03-25 19:01:17 +0300

    adding set editable

---
+++ build/precache-manifest.5f6287965cb919612ede65713f92d5a3.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "9311363c8afa31a3562b",
+    "url": "/static/css/main.2794befe.css"
+  },
+  {
+    "revision": "9311363c8afa31a3562b",
+    "url": "/static/js/main.9311363c.js"
+  },
+  {
+    "revision": "306d8df30b13d31454b47655af2a3795",
+    "url": "/index.html"
+  }
+];
