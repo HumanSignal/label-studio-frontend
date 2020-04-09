9d7aa7eeb0ee04466e326b8b05df9d9eb8a62138
Mikhail Maluyk <mikhail.maluyk@gmail.com>
2020-04-09 08:50:14 +0300

    TS build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed362bfcdec8569895c3dccf27ae87d9.js"
+  "/precache-manifest.0d11e4c429ab362cb09a73d2f87d49ec.js"
 );
 
 workbox.clientsClaim();
