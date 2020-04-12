1f0b9b65566eab92b52fd88dcac3c59e5d7cde73
Michael Malyuk <mikhail.maluyk@gmail.com>
2020-04-12 10:42:24 -0700

    Update development.js

--- src/env/development.js
+++ src/env/development.js
@@ -39,7 +39,7 @@ import { Pairwise } from "../examples/pairwise"; // eslint-disable-line no-unuse
 /**
  * Custom Data
  */
-import { AllTypes } from "../examples/all_types"; // eslint-disable-line no-unused-vars
+// import { AllTypes } from "../examples/all_types"; // eslint-disable-line no-unused-vars
 
 const data = NamedEntity;
 
