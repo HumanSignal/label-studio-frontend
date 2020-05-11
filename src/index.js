a635fa8ecf85437e7bdee814f594a50688fcfd4d
hlomzik <hlomzik@gmail.com>
2020-05-11 23:14:15 +0700

    Revert index.js

--- src/index.js
+++ src/index.js
@@ -6,37 +6,92 @@ import "./assets/styles/global.scss";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App/App";
 import AppStore from "./stores/AppStore";
-import ProductionEnvironment from "./env/production";
+import DevelopmentEnvironment from "./env/development";
+import ProductionEnviroment from "./env/production";
 
-let environment = ProductionEnvironment;
+let enviroment = DevelopmentEnvironment;
 
-window.LabelStudio = (element, options) => {
-  let params = options;
+if (process.env.NODE_ENV === "production") {
+  enviroment = ProductionEnviroment;
 
-  if (params && params.task) {
-    params.task = environment.getData(params.task);
-  }
+  window.LabelStudio = (element, options) => {
+    let params = options;
 
-  /**
-   * Configure Application
-   */
-  const app = AppStore.create(params, environment.configureApplication(params));
+    if (params && params.task) {
+      params.task = enviroment.getData(params.task);
+    }
 
-  /**
-   * Initialize store
-   */
-  app.initializeStore(environment.getState(params.task));
+    /**
+     * Configure Application
+     */
+    const app = AppStore.create(params, enviroment.configureApplication(params));
 
-  ReactDOM.render(
-    <Provider store={app}>
-      <App />
-    </Provider>,
-    environment.rootElement(element),
-  );
+    /**
+     * Initialize store
+     */
+    app.initializeStore(enviroment.getState(params.task));
 
-  window.Htx = app;
-  return app;
-};
+    ReactDOM.render(
+      <Provider store={app}>
+        <App />
+      </Provider>,
+      enviroment.rootElement(element),
+    );
+
+    window.Htx = app;
+    return app;
+  };
+} else {
+  enviroment = DevelopmentEnvironment;
+
+  window.LabelStudio = (element, options) => {
+    let params = options;
+
+    // this is a way to initialize one of the examples from the src/examples section
+    if (!options.config) {
+      enviroment.getExample().then(result => {
+        params = {
+          ...params,
+          ...result,
+        };
+
+        let app = AppStore.create(params, enviroment.configureApplication(params));
+
+        app.initializeStore({ completions: [params.completion], predictions: params.predictions });
+        window.Htx = app;
+
+        ReactDOM.render(
+          <Provider store={app}>
+            <App />
+          </Provider>,
+          enviroment.rootElement(element),
+        );
+      });
+    } else {
+      // this is static initialization from the index.html file
+      params = {
+        ...params,
+        task: {
+          ...params.task,
+          data: JSON.stringify(params.task.data),
+        },
+      };
+
+      let app = AppStore.create(params, enviroment.configureApplication(params));
+
+      app.initializeStore({ completions: params.task.completions, predictions: params.task.predictions });
+
+      ReactDOM.render(
+        <Provider store={app}>
+          <App />
+        </Provider>,
+        enviroment.rootElement(element),
+      );
+
+      return app;
+    }
+  };
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
