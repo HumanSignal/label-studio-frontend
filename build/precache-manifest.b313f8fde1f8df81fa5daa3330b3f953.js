983502e870a564f1c5634bfa88db94532741240b
Mikhail Maluyk <mikhail.maluyk@gmail.com>
2020-05-11 09:47:20 +0300

    latest build

---
+++ build/precache-manifest.b313f8fde1f8df81fa5daa3330b3f953.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "92b2b1e6dd8ebc17b60c",
+    "url": "/static/css/main.73fe1ae5.css"
+  },
+  {
+    "revision": "92b2b1e6dd8ebc17b60c",
+    "url": "/static/js/main.92b2b1e6.js"
+  },
+  {
+    "revision": "b7b77a3135b91d4aa625dd5798c73fba",
+    "url": "/index.html"
+  }
+];
