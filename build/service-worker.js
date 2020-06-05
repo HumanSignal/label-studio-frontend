1fa4ef67facf059e297c21bfa8343a8aeb90b9d5
hlomzik <hlomzik@gmail.com>
2020-06-06 00:21:00 +0300

    0.7.1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2bef322d091601b56f65bd1db564c07.js"
+  "/precache-manifest.0d9b98aa923f2b8f03a9e5c35ae50505.js"
 );
 
 workbox.clientsClaim();
