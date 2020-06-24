0d63e788b13caf0f6d03083763be9311e594f418
hlomzik <hlomzik@gmail.com>
2020-06-24 17:11:16 +0300

    Mark skipped completions with icon

--- src/components/Completions/Completions.js
+++ src/components/Completions/Completions.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Card, Button, Tooltip, Badge, List, Popconfirm } from "antd";
 import { observer } from "mobx-react";
-import { StarOutlined, DeleteOutlined, WindowsOutlined, PlusOutlined } from "@ant-design/icons";
+import { StarOutlined, DeleteOutlined, DisconnectOutlined, WindowsOutlined, PlusOutlined } from "@ant-design/icons";
 
 import Utils from "../../utils";
 import styles from "./Completions.module.scss";
 
 const Completion = observer(({ item, store }) => {
+  // @todo remove honeypot/ground truth
   let removeHoney = () => (
     <Tooltip placement="topLeft" title="Unset this result as a ground truth">
       <Button
@@ -84,7 +85,11 @@ const Completion = observer(({ item, store }) => {
 
     return (
       <div className={styles.buttons}>
-        {true && (item.honeypot ? removeHoney() : setHoney())}
+        {store.hasInterface("skip") && item.skipped && (
+          <Tooltip placement="topLeft" title="Skipped completion">
+            <DisconnectOutlined className={styles.skipped} />
+          </Tooltip>
+        )}
         &nbsp;
         {store.hasInterface("completions:delete") && (
           <Tooltip placement="topLeft" title="Delete selected completion">
