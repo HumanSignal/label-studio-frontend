48de385812eb0926091121d8c894077b88f93340
hlomzik <hlomzik@gmail.com>
2020-07-09 18:37:46 +0300

    0.7.3

---
+++ build/precache-manifest.83c9633792aeb3b75a607097c7caa55c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64a52e2fb493894832ff4e0a1e75a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a2bfc0d4fe662676631",
+    "url": "/static/css/main.09b8161e.css"
+  },
+  {
+    "revision": "7a2bfc0d4fe662676631",
+    "url": "/static/js/main.e963e015.js"
+  },
+  {
+    "revision": "5a68f21b3cb3c487a0a1de2274234479",
+    "url": "/static/js/main.e963e015.js.LICENSE.txt"
+  }
+]);
