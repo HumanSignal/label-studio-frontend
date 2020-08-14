584e40a2f6db031f916bcfe15f9e5af3bd5bfb6f
hlomzik <hlomzik@gmail.com>
2020-08-14 17:00:53 +0300

    Don't save empty draft right after init

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -254,7 +254,8 @@ export default types
 
       return new Promise(resolve => {
         const fn = getEnv(self).onSubmitDraft;
-        if (!fn) return resolve();
+        if (!fn || !c.regionStore) return resolve();
+        if (!c.pk && !c.regionStore.regions.length) return resolve();
         const res = fn(self, c);
         if (res && res.then) res.then(resolve);
         else resolve(res);
