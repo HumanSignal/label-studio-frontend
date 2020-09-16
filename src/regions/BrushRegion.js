0362116bb2aaf170da62040ab0f4fb92a9767de7
hlomzik <hlomzik@gmail.com>
2020-09-16 12:15:55 +0300

    Fix Brush regions detection

--- src/regions/BrushRegion.js
+++ src/regions/BrushRegion.js
@@ -349,6 +349,6 @@ const HtxBrushView = ({ store, item }) => {
 const HtxBrush = inject("store")(observer(HtxBrushView));
 
 Registry.addTag("brushregion", BrushRegionModel, HtxBrush);
-Registry.addRegionType(BrushRegionModel, "image");
+Registry.addRegionType(BrushRegionModel, "image", value => value.rle);
 
 export { BrushRegionModel, HtxBrush };
