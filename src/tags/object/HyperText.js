1b8d04f40c82a23435acdc23aed9ac54bd1ea997
hlomzik <hlomzik@gmail.com>
2020-09-29 15:46:41 +0300

    Same fix for spans in HyperText

--- src/tags/object/HyperText.js
+++ src/tags/object/HyperText.js
@@ -235,7 +235,11 @@ class HyperTextPieceView extends Component {
     const { item } = this.props;
 
     item.regs.forEach(function(r) {
-      if (r._spans && r._spans.length) return;
+      // spans can be totally missed if this is app init or undo/redo
+      // or they can be disconnected from DOM on completions switching
+      // so we have to recreate them from regions data
+      if (r._spans?.[0]?.isConnected) return;
+
       try {
         const range = xpath.toRange(r.start, r.startOffset, r.end, r.endOffset, root);
 
