48c8cfa261a45dbd6001e2adf3bfedac3a25f36d
hlomzik <hlomzik@gmail.com>
2020-10-16 14:55:06 +0300

    0.7.5

---
+++ build/precache-manifest.b6ebb020b6cbfa8ea75cee3e0d4f34d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbb8ca8f586797dfcb374b59cf593fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2351d6016c133fef176b",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "2351d6016c133fef176b",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "d9c5248aef5b6070ac286f3b4ec8406a",
+    "url": "/static/js/main.js.LICENSE.txt"
+  }
+]);
