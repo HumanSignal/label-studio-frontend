502d62350fdcf4fac5aa12f4ad3826da33825a6c
hlomzik <hlomzik@gmail.com>
2020-10-26 23:00:23 +0300

    Fix doc about `Channel.format`

--- src/tags/object/TimeSeries/Channel.js
+++ src/tags/object/TimeSeries/Channel.js
@@ -16,14 +16,14 @@ import { errorBuilder } from "../../../core/DataValidator/ConfigValidator";
  * @param {string} column column name or index
  * @param {string} [legend] display name of the channel
  * @param {string} [units] display units name
- * @param {string} [displayFormat] format string for the values, uses d3-format:
- *        [,][.precision][f|%]
- *        , - group thousands with separator (from locale): , (12345.6 -> 12,345.6) ,.2f (12345.6 -> 12,345.60)
- *        .precision - precision for `f|%` type, significant digits for empty type:
- *                     .3f (12.3456 -> 12.345, 1000 -> 1000.000)
- *                     .3 (12.3456 -> 12.3, 1.2345 -> 1.23, 12345 -> 1.23e+4)
- *        f - treat as float, default precision is .6: f (12 -> 12.000000) .2f (12 -> 12.00) .0f (12.34 -> 12)
- *        % - treat as percents and format accordingly: %.0 (0.128 -> 13%) %.1 (1.2345 -> 123.4%)
+ * @param {string} [displayFormat] format string for the values, uses d3-format:<br/>
+ *        `[,][.precision][f\|%]`<br/>
+ *        `,` - group thousands with separator (from locale): `,` (12345.6 -> 12,345.6) `,.2f` (12345.6 -> 12,345.60)<br/>
+ *        `.precision` - precision for `f\|%` type, significant digits for empty type:<br/>
+ *                     `.3f` (12.3456 -> 12.345, 1000 -> 1000.000)<br/>
+ *                     `.3` (12.3456 -> 12.3, 1.2345 -> 1.23, 12345 -> 1.23e+4)<br/>
+ *        `f` - treat as float, default precision is .6: `f` (12 -> 12.000000) `.2f` (12 -> 12.00) `.0f` (12.34 -> 12)<br/>
+ *        `%` - treat as percents and format accordingly: `%.0` (0.128 -> 13%) `%.1` (1.2345 -> 123.4%)
  * @param {number} [height] height of the plot
  * @param {string=} [strokeColor=#f48a42] plot stroke color, expects hex value
  * @param {number=} [strokeWidth=1] plot stroke width
