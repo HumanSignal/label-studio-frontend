e62172534efbb4fda95aa84c57f5bbeabdcb3d41
hlomzik <hlomzik@gmail.com>
2020-11-13 01:21:54 +0300

    Fix deserialization of tree view in Regions panel

--- src/regions/Result.js
+++ src/regions/Result.js
@@ -199,6 +199,10 @@ const Result = types
         data.meta = { ...data.meta, ...meta };
       }
 
+      if (self.area.parentID) {
+        data.parentID = self.area.parentID.replace(/#.*/, "");
+      }
+
       Object.assign(data, { id, from_name, to_name, type });
       value[valueType] && Object.assign(data.value, { [valueType]: value[valueType] });
       if (typeof score === "number") data.score = score;
