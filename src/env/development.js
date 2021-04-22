c49cbaa4c417b09ca407eae6f4e7e37b90b2d330
Nikita Skryabin <nr@fenelon.ru>
2021-04-22 04:46:00 +0300

    Fix padding

--- src/env/development.js
+++ src/env/development.js
@@ -50,7 +50,7 @@ import { TimeSeries } from "../examples/timeseries";
  */
 // import { AllTypes } from "../examples/all_types";
 
-const data = ImageBbox;
+const data = NamedEntity;
 
 function getData(task) {
   if (task && task.data) {
