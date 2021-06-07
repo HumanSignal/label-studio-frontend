22a7cd7c715e4d2a8c3b79095b0126357bc921b9
Sergey <Gondragos@gmail.com>
2021-06-07 12:56:48 +0300

    Fix for selecting regions after drawing (#207)
    * Add test for selectiong regions after drawing
    
    * Fix selecting after drawing

--- e2e/fragments/AtImageView.js
+++ e2e/fragments/AtImageView.js
@@ -1,30 +1,43 @@
 /* global inject */
 const { I } = inject();
 
-const {
-  waitForImage,
-  clickKonva,
-  polygonKonva,
-  clickMultipleKonva,
-  dragKonva,
-  hasKonvaPixelColorAtPoint,
-  whereIsPixel,
-  getCanvasSize,
-  setZoom,
-} = require("../tests/helpers");
+const Helpers = require("../tests/helpers");
 
 module.exports = {
+  _stageSelector: ".konvajs-content",
+  _stageBBox: { x: 0, y: 0, width: 0, height: 0 },
+
+  async grabStageBBox() {
+    const bbox = await I.grabElementBoundingRect(this._stageSelector);
+    return bbox;
+  },
+
+  async lookForStage() {
+    const bbox = await I.grabElementBoundingRect(this._stageSelector);
+    this._stageBBox = bbox;
+  },
+
   waitForImage() {
-    I.executeAsyncScript(waitForImage);
+    I.executeAsyncScript(Helpers.waitForImage);
   },
 
   async getCanvasSize() {
-    const sizes = await I.executeAsyncScript(getCanvasSize);
+    const sizes = await I.executeAsyncScript(Helpers.getCanvasSize);
+    return sizes;
+  },
+
+  async getImageSize() {
+    const sizes = await I.executeAsyncScript(Helpers.getImageSize);
+    return sizes;
+  },
+
+  async getImageFrameSize() {
+    const sizes = await I.executeAsyncScript(Helpers.getImageFrameSize);
     return sizes;
   },
 
   setZoom(scale, x, y) {
-    I.executeAsyncScript(setZoom, scale, x, y);
+    I.executeAsyncScript(Helpers.setZoom, scale, x, y);
   },
 
   /**
@@ -33,21 +46,22 @@ module.exports = {
    * @param {number} y
    */
   clickKonva(x, y) {
-    I.executeAsyncScript(clickKonva, x, y);
+    I.executeAsyncScript(Helpers.clickKonva, x, y);
   },
   /**
    * Click multiple times on the main Stage
    * @param {number[][]} points
    */
   clickPointsKonva(points) {
-    I.executeAsyncScript(clickMultipleKonva, points);
+    I.executeAsyncScript(Helpers.clickMultipleKonva, points);
   },
   /**
    * Click multiple times on the main Stage then close Polygon
    * @param {number[][]} points
+   * @deprecated Use drawByClickingPoints instead
    */
   clickPolygonPointsKonva(points) {
-    I.executeAsyncScript(polygonKonva, points);
+    I.executeAsyncScript(Helpers.polygonKonva, points);
   },
   /**
    * Dragging between two points
@@ -55,10 +69,12 @@ module.exports = {
    * @param {number} y
    * @param {number} shiftX
    * @param {number} shiftY
+   * @deprecated Use drawByDrag instead
    */
   dragKonva(x, y, shiftX, shiftY) {
-    I.executeAsyncScript(dragKonva, x, y, shiftX, shiftY);
+    I.executeAsyncScript(Helpers.dragKonva, x, y, shiftX, shiftY);
   },
+
   /**
    * Get  pixel color at point
    * @param {number} x
@@ -67,12 +83,76 @@ module.exports = {
    * @param {number} tolerance
    */
   async hasPixelColor(x, y, rgbArray, tolerance = 3) {
-    const hasPixel = await I.executeAsyncScript(hasKonvaPixelColorAtPoint, x, y, rgbArray, tolerance);
+    const hasPixel = await I.executeAsyncScript(Helpers.hasKonvaPixelColorAtPoint, x, y, rgbArray, tolerance);
     return hasPixel;
   },
   // Only for debugging
   async whereIsPixel(rgbArray, tolerance = 3) {
-    const points = await I.executeAsyncScript(whereIsPixel, rgbArray, tolerance);
+    const points = await I.executeAsyncScript(Helpers.whereIsPixel, rgbArray, tolerance);
     return points;
+  },
+
+  async countKonvaShapes() {
+    const count = await I.executeAsyncScript(Helpers.countKonvaShapes);
+    return count;
+  },
+
+  /**
+   * Mousedown - mousemove - mouseup drawing on the ImageView. Works in couple of lookForStage.
+   * @example
+   * async AtImageView.lookForStage();
+   * AtImageView.drawByDrag(50, 30, 200, 200);
+   * @param x
+   * @param y
+   * @param shiftX
+   * @param shiftY
+   */
+  drawByDrag(x, y, shiftX, shiftY) {
+    I.moveMouse(this._stageBBox.x + x, this._stageBBox.y + y);
+    I.pressMouseDown();
+    I.moveMouse(this._stageBBox.x + x + shiftX, this._stageBBox.y + y + shiftY, 3);
+    I.pressMouseUp();
+  },
+  /**
+   * Click through the list of points on the ImageView. Works in couple of lookForStage.
+   * @example
+   * async AtImageView.loolookkForStage();
+   * AtImageView.drawByClickingPoints([[50,50],[100,50],[100,100],[50,100],[50,50]]);
+   * @param {number[][]} points
+   */
+  drawByClickingPoints(points) {
+    for (const point of points) {
+      I.clickAt(this._stageBBox.x + point[0], this._stageBBox.y + point[1]);
+    }
+  },
+  /**
+   * Mousedown - mousemove - mouseup drawing through the list of points on the ImageView. Works in couple of lookForStage.
+   * @example
+   * async AtImageView.lookForStage();
+   * AtImageView.drawThroughPoints([[50,50],[200,100],[50,200],[300,300]]);
+   * @param {number[][]} points - list of pairs of coords
+   * @param {"steps"|"rate"} mode - mode of firing mousemove event
+   * @param {number} parameter - parameter for mode
+   */
+  drawThroughPoints(points, mode = "steps", parameter = 1) {
+    const calcSteps = {
+      steps: () => parameter,
+      rate: (p1, p2) => Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2)) / parameter,
+    }[mode];
+    const startPoint = points[0];
+    I.moveMouse(this._stageBBox.x + startPoint[0], this._stageBBox.y + startPoint[1]);
+    I.pressMouseDown();
+    for (let i = 1; i < points.length; i++) {
+      const prevPoint = points[i - 1];
+      const curPoint = points[i];
+      I.moveMouse(this._stageBBox.x + curPoint[0], this._stageBBox.y + curPoint[1], calcSteps(prevPoint, curPoint));
+    }
+    I.pressMouseUp();
+  },
+  clickAt(x, y) {
+    I.clickAt(this._stageBBox.x + x, this._stageBBox.y + y);
+  },
+  drawByClick(x, y) {
+    this.clickAt(x, y);
   },
 };
