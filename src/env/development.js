74f5ced034033732e58a2cff5e3bee319b7dd99d
Nikita Skryabin <nr@fenelon.ru>
2021-06-20 08:14:39 +0300

    Escape HTML in Text tag

--- src/env/development.js
+++ src/env/development.js
@@ -60,7 +60,7 @@ import { TimeSeriesSingle } from "../examples/timeseries_single";
  */
 // import { AllTypes } from "../examples/all_types";
 
-const data = ImageBbox;
+const data = RichTextPlain;
 
 function getData(task) {
   if (task && task.data) {
