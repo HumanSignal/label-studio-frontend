a9feeb24adbea245bac7945cc2e769580620e61a
Nikita Skryabin <nr@fenelon.ru>
2021-06-20 07:20:57 +0300

    Use full shape

--- e2e/tests/regression-tests/image-ctrl-drawing.test.js
+++ e2e/tests/regression-tests/image-ctrl-drawing.test.js
@@ -66,6 +66,7 @@ const createShape = {
       points.push([x + width, y]);
       points.push([x + width, y + height]);
       points.push([x, y + height]);
+      points.push([x, y]);
       return {
         ...opts,
         action: "drawByClickingPoints",
