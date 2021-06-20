f4d1ffbead62f24a5e3aaa37b039d35f74f469d4
Nikita Skryabin <nr@fenelon.ru>
2021-06-21 00:12:58 +0300

    Remove ground truth from generated annotations

--- src/components/CurrentEntity/GroundTruth.js
+++ src/components/CurrentEntity/GroundTruth.js
@@ -12,7 +12,7 @@ export const GroundTruth = observer(({entity}) => {
     ? "Unset this result as a ground truth"
     : "Set this result as a ground truth";
 
-  return (!entity.skipped) && (
+  return (!entity.skipped && !entity.userGenerate) && (
     <Block name="ground-truth">
       <Tooltip placement="topLeft" title={title}>
         <Elem
