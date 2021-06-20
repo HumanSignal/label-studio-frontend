63770857fb95dacbcc9eab72eb58c72969defc71
Nikita Skryabin <nr@fenelon.ru>
2021-06-20 06:59:59 +0300

    New events system

--- src/LabelStudio.js
+++ src/LabelStudio.js
@@ -4,6 +4,10 @@ import { configureStore } from "./configureStore";
 import { LabelStudio as LabelStudioReact } from './Component';
 import { registerPanels } from "./registerPanels";
 import { configure } from "mobx";
+import {EventInvoker} from './utils/events';
+import legacyEvents from './core/External';
+import { toCamelCase } from "strman";
+import { isDefined } from "./utils/utilities";
 
 configure({
   isolateGlobalState: true,
@@ -19,6 +23,8 @@ export class LabelStudio {
 
   constructor (root, options = {}) {
     this.root = root;
+    this.events = new EventInvoker();
+    this.supportLgacyEvents(options);
     this.options = options ?? {};
     this.destroy = (() => { /* noop */ });
     this.createApp();
@@ -26,8 +32,16 @@ export class LabelStudio {
     this.constructor.instances.add(this);
   }
 
+  on(...args) {
+    this.events.on(...args);
+  }
+
+  off(...args){
+    this.events.off(...args);
+  }
+
   async createApp() {
-    const {store, getRoot} = await configureStore(this.options);
+    const {store, getRoot} = await configureStore(this.options, this.events);
     const rootElement = getRoot(this.root);
     this.store = store;
     window.Htx = this.store;
@@ -44,6 +58,20 @@ export class LabelStudio {
     };
 
     this.destroy = destructor;
+  }
+
+  supportLgacyEvents(options) {
+    console.log({legacyEvents});
+    const keys = Object.keys(legacyEvents);
+
+    keys.forEach(key => {
+      const callback = options[key];
+
+      if (isDefined(callback)) {
+        const eventName = toCamelCase(key.replace(/^on/, ''));
+        this.events.on(eventName, callback);
+      }
+    });
   }
 }
 
