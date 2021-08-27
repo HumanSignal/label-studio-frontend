0028adf305dbb6f206a589e4047b46661d8cb0b2
hlomzik <hlomzik@gmail.com>
2021-08-27 17:24:59 +0300

    [fix] no-referrer for iframe in HyperText

--- src/tags/object/RichText/view.js
+++ src/tags/object/RichText/view.js
@@ -227,6 +227,7 @@ class RichTextPieceView extends Component {
       return (
         <ObjectTag item={item}>
           <iframe
+            referrerpolicy="no-referrer"
             sandbox="allow-same-origin allow-scripts"
             ref={this.rootNodeRef}
             style={style}
@@ -235,6 +236,7 @@ class RichTextPieceView extends Component {
             onLoad={this.onIFrameLoad}
           />
           <iframe
+            referrerpolicy="no-referrer"
             sandbox="allow-same-origin allow-scripts"
             ref={this.originalContentRef}
             className="htx-richtext-orig"
