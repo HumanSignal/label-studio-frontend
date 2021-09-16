7810174cf8e3e96a0ad200a1d2842d41370dfd3b
Nick <nr@fenelon.ru>
2021-09-16 15:14:15 +0300

    Remove excess code

--- src/mixins/ToolManagerMixin.js
+++ src/mixins/ToolManagerMixin.js
@@ -1,18 +1,10 @@
-import { getRoot, types } from "mobx-state-tree";
+import { types } from "mobx-state-tree";
 import ToolsManager from "../tools/Manager";
 import * as Tools from '../tools';
 
 export const ToolManagerMixin = types.model().actions((self) => {
-  const Super = {
-    afterAttach: self.afterAttach ?? (() => {}),
-  };
-
   return {
     afterAttach() {
-      Super.afterAttach();
-
-      console.log(getRoot(self));
-
       const toolNames = self.toolNames ?? [];
       const manager = ToolsManager.getInstance({ name: self.toname });
       const env = { manager, control: self };
