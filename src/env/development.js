ea90bd52150ceed5efe3dc1e931be5f2f949e934
Nick <nr@fenelon.ru>
2021-10-15 06:41:04 +0300

    Remove old tags

--- src/env/development.js
+++ src/env/development.js
@@ -62,7 +62,7 @@ import { TimeSeriesSingle } from "../examples/timeseries_single";
  */
 // import { AllTypes } from "../examples/all_types";
 
-const data = TimeSeries;
+const data = Dialogue;
 
 function getData(task) {
   if (task && task.data) {
