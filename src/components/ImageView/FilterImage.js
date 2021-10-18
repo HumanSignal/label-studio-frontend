4362673e7daf5a32b70868e462dbac2edc4bc5d7
twsl <45483159+twsl@users.noreply.github.com>
2021-10-18 11:48:53 +0000

    Removed logging and default filter

--- src/components/ImageView/FilterImage.js
+++ src/components/ImageView/FilterImage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Konva from "konva";
 import { Image } from "react-konva";
 
+Konva.pixelRatio = 1;
+
 // Adapter from: https://github.com/konvajs/use-image/blob/master/index.js
 const useImage = (url, crossOrigin, onLoad, onError) => {
   var defaultState = { image: undefined, status: 'loading' };
@@ -57,24 +59,10 @@ export const FilterImage = ({ url, crossOrigin, onLoad, onError, filters, ...pro
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/crossorigin
   const [image, status] = useImage(url, crossOrigin || 'anonymous', onLoad, onError);
 
-  // const getBase64 = async (url) => {
-  //   try {
-  //     let image = await axios.get(url, { responseType: 'arraybuffer' });
-  //     let raw = Buffer.from(image.data).toString('base64');
-
-  //     return "data:" + image.headers["content-type"] + ";base64," + raw;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   // when image is loaded we need to cache the shape
   React.useEffect(() => {
-    console.log("image changed, val: " + image);
     if (image) {
       // you many need to reapply cache on some props changes like shadow, stroke, etc.
-      console.log("Cache image: " + image);
-      console.log("Image loaded: " + image.complete);
       imageRef.current.cache();
       //imageRef.current.getLayer().draw();
     }
@@ -92,7 +80,7 @@ export const FilterImage = ({ url, crossOrigin, onLoad, onError, filters, ...pro
           x={0}
           y={0}
           image={image}
-          filters={filters || [Konva.Filters.Grayscale]}
+          filters={filters}
           {...props}
         />
       )}
