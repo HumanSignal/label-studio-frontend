202d81159adab5af72eab2c5bd5fa9810664cb31
twsl <45483159+twsl@users.noreply.github.com>
2021-10-19 23:00:48 +0000

    Improve model form

--- src/tools/Filter.js
+++ src/tools/Filter.js
@@ -10,17 +10,28 @@ import ToolMixin from "../mixins/Tool";
 import { Tool } from "../components/Toolbar/Tool";
 //import { Range } from "../common/Range/Range";
 import { FilterOutlined } from "@ant-design/icons";
-import { Col, Collapse, Empty, Modal, Row, Select } from "antd";
+import { Col, Collapse, Empty, Form, Modal, Row, Select } from "antd";
+import { SettingOutlined } from '@ant-design/icons';
 
 const FilterView = observer(({ item }) => {
   const [modalVisible, setModelVisible] = useState(false);
   const [selectedItems, setSelectedItems] = useState([]);
+
+  const [form] = Form.useForm();
 
   const OPTIONS = Object.keys(Konva.Filters);
   const handleChange = selectedItems => {
     setSelectedItems(selectedItems);
   };
   const filteredOptions = OPTIONS.filter(o => !selectedItems.includes(o));
+  const genExtra = () => (
+    <SettingOutlined
+      onClick={event => {
+        // If you don't want click extra trigger collapse, you can prevent this:
+        event.stopPropagation();
+      }}
+    />
+  );
 
   return (
     <>
@@ -36,7 +47,7 @@ const FilterView = observer(({ item }) => {
           //item.setMode(sel);
         }} />
       <Modal
-        title="Vertically centered modal dialog"
+        title="Filter Settings"
         centered
         visible={modalVisible}
         onOk={() => {
@@ -48,35 +59,44 @@ const FilterView = observer(({ item }) => {
       >
         <Row>
           <Col span={24}>
-            <Select
-              key="filters"
-              mode="multiple"
-              allowClear
-              style={{ width: '100%' }}
-              placeholder="Please select filter(s)"
-              value={selectedItems}
-              onChange={handleChange}
+            <Form
+              form={form}
+              layout="vertical"
             >
-              {filteredOptions.map(item => (
-                <Select.Option key={item} value={item}>
-                  {item}
-                </Select.Option>
-              ))}
-            </Select >
-            {selectedItems.length > 0 ? (
-              <Collapse
-                defaultActiveKey={selectedItems[0]}
-                // expandIconPosition={'left'}
-                accordion
-              >
-                {selectedItems.map(item => (
-                  <Collapse.Panel header={item} key={item}>
-                    <div>{item}</div>
-                    <p>TODO: Render options...</p>
-                  </Collapse.Panel>
-                ))}
-              </Collapse>
-            ) : (<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />)}
+              <Form.Item label="Select Filters" name="selectFilters">
+                <Select
+                  key="filters"
+                  mode="multiple"
+                  allowClear
+                  style={{ width: '100%' }}
+                  placeholder="Please select filter(s)"
+                  value={selectedItems}
+                  onChange={handleChange}
+                >
+                  {filteredOptions.map(item => (
+                    <Select.Option key={item} value={item}>
+                      {item}
+                    </Select.Option>
+                  ))}
+                </Select >
+              </Form.Item>
+              <Form.Item label="Configure Filters" name="configureFilters">
+                {selectedItems.length > 0 ? (
+                  <Collapse
+                    defaultActiveKey={selectedItems[0]}
+                    // expandIconPosition={'left'}
+                    accordion
+                  >
+                    {selectedItems.map(item => (
+                      <Collapse.Panel header={`${item} Settings`} key={item} extra={genExtra()}>
+                        <div>{item}</div>
+                        <p>TODO: Render options...</p>
+                      </Collapse.Panel>
+                    ))}
+                  </Collapse>
+                ) : (<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />)}
+              </Form.Item>
+            </Form>
           </Col>
         </Row>
       </Modal>
