a65c16f1a0eac84912b7e8f79c25179276373992
makseq-ubnt <makseq@gmail.com>
2021-10-29 04:29:28 +0300

    [fix] Review bugs

--- src/stores/AnnotationStore.js
+++ src/stores/AnnotationStore.js
@@ -43,6 +43,7 @@ const Annotation = types
     user: types.optional(types.maybeNull(types.safeReference(UserExtended)), null),
     parent_prediction: types.maybeNull(types.integer),
     parent_annotation: types.maybeNull(types.integer),
+    last_annotation_history: types.maybeNull(types.integer),
 
     loadedDate: types.optional(types.Date, new Date()),
     leadTime: types.maybeNull(types.number),
