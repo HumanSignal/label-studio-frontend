e829d02014b366a8ecdeae8ba8163286e9ab198d
Koshevarov Sergey <gondragos@gmail.com>
2021-11-18 01:11:46 +0300

    fix tests

--- e2e/tests/image.gestures.test.js
+++ e2e/tests/image.gestures.test.js
@@ -205,7 +205,7 @@ Scenario("Creating regions by various gestures", async function({ I, AtImageView
   for (const [idx, region] of Object.entries(regions)) {
     I.pressKey(region.hotKey);
     AtImageView[region.action](...region.params);
-    AtSidebar.seeRegions(idx+1);
+    AtSidebar.seeRegions(+idx+1);
   }
   const result = await I.executeScript(serialize);
 
