d21830f7eb3108bffcdcc11665be9a283cad917a
hlomzik <hlomzik@gmail.com>
2021-11-19 19:30:56 +0300

    [fix] showLabels=false takes precedence

--- src/mixins/HighlightMixin.js
+++ src/mixins/HighlightMixin.js
@@ -168,7 +168,7 @@ export const HighlightMixin = types
     getLabels() {
       const settings = getRoot(self).settings;
 
-      if (!self.parent.showlabels && !settings.showLabels) return null;
+      if (!self.parent.showlabels || !settings.showLabels) return null;
 
       return self.labeling?.mainValue ?? [];
     },
