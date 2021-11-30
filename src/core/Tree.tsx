58e8c06e3a53badecf19d8a5f91948af5b18660a
Nick Skriabin <767890+nicholasrq@users.noreply.github.com>
2021-12-01 02:07:08 +0300

    Fix repeater tag (#375)
    * Bring back attributes parsing for the repeater tag
    
    * Remove traces
    
    * Remove trace
    
    * Fix index flag

--- src/core/Tree.tsx
+++ src/core/Tree.tsx
@@ -30,6 +30,34 @@ function detectParseError(doc?: Document) {
   }
 }
 
+const deepReplaceAttributes = (
+  root: Element,
+  idx: number,
+  indexFlag: string,
+) => {
+  function recursiveClone(node: Element) {
+    if (node.attributes === undefined) return;
+
+    const attrNames = Array.from(node.attributes).map(att => att.name);
+
+    for (const name of attrNames) {
+      const value = node.getAttribute(name);
+
+      if (name === '$$') {
+        node.setAttribute(name, value?.map(c => recursiveClone(c)));
+      } else if (name === '$') {
+        node.setAttribute(name, recursiveClone(value));
+      } else if (typeof name === 'string') {
+        node.setAttribute(name, value?.replace?.(indexFlag, `${idx}`) ?? "");
+      }
+    }
+
+    node.childNodes.forEach(node => recursiveClone(node as Element));
+  }
+
+  recursiveClone(root);
+};
+
 function tagIntoObject(
   node: Element,
   taskData: Record<string, any>,
@@ -37,6 +65,7 @@ function tagIntoObject(
 ): ConfigNode {
   const props = attrsToProps(node, replaces);
   const type = node.tagName.toLowerCase();
+  const indexFlag = props.indexflag ?? "{{idx}}";
   const data: ConfigNode = {
     ...props,
     id: guidGenerator(),
@@ -49,12 +78,18 @@ function tagIntoObject(
     const views = [];
 
     for (let i = 0; i < repeaterArray.length; i++) {
-      const newReplaces: Record<string, string> = { ...replaces, [props.indexflag ?? "{{idx}}"]: i };
+      const newReplaces: Record<string, string> = { ...replaces, [indexFlag]: i };
       const view = {
         id: guidGenerator(),
         tagName: "View",
         type: "view",
-        children: [...node.children].map(child => tagIntoObject(child, taskData, newReplaces)),
+        children: [...node.children].map(child => {
+          const clonedNode = child.cloneNode(true) as Element;
+
+          deepReplaceAttributes(clonedNode, i, indexFlag);
+
+          return tagIntoObject(clonedNode, taskData, newReplaces);
+        }),
       };
 
       views.push(view);
