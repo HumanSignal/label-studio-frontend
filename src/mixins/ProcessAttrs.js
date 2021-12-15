08e75458b14fd50d38457bf92392210126d1d4c4
hlomzik <hlomzik@gmail.com>
2021-12-15 17:24:34 +0300

    Add `resolver` param to retrieve data from remote
    Currently only csv for Text/HyperText are supported

--- src/mixins/ProcessAttrs.js
+++ src/mixins/ProcessAttrs.js
@@ -1,15 +1,64 @@
-import { types } from "mobx-state-tree";
+import { flow, types } from "mobx-state-tree";
 
-import { parseValue } from "../utils/data";
+import { parseCSV, parseValue } from "../utils/data";
 
-const ProcessAttrsMixin = types.model().actions(self => ({
-  updateLocalValue(value) {
-    self._value = value;
+const resolvers = {
+  // @todo comments/types
+  csv(content, options = {}) {
+    const [results, names] = parseCSV(content, options.separator);
+    const { column = names[0] } = options;
+
+    return String(results[column][0]);
   },
+};
 
-  updateValue(store) {
-    self._value = parseValue(self.value, store.task.dataObj);
-  },
-}));
+const ProcessAttrsMixin = types
+  .model({
+    resolver: types.maybeNull(types.string),
+  })
+  .actions(self => ({
+    updateLocalValue(value) {
+      self._value = value;
+    },
+
+    updateValue(store) {
+      self._value = parseValue(self.value, store.task.dataObj);
+    },
+
+    /**
+     * Use `resolver` param for data retrieval from remote resource
+     * format: <type>(<separator>option=value)*
+     * currently only csv type supported, separator is | by default
+     */
+    resolveValue: flow(function * (value) {
+      if (!self.resolver) return value;
+
+      const [, type, sep] = self.resolver.match(/^(\w+)(.)?/) ?? [];
+
+      if (!Object.prototype.hasOwnProperty.call(resolvers, type)) {
+        console.error(`Resolver "${type ?? self.resolver}" looks unfamiliar`);
+        return value;
+      }
+
+      const options = {};
+
+      if (sep) {
+        const pairs = self.resolver.split(sep).slice(1);
+
+        pairs.forEach(pair => {
+          const [k, v] = pair.split("=", 2);
+
+          options[k] = v;
+        });
+      }
+
+      // @todo checks for url
+      // @todo error handling
+      const response = yield fetch(value);
+      const text = yield response.text();
+
+      return resolvers[type](text, options);
+    }),
+  }));
 
 export default ProcessAttrsMixin;
