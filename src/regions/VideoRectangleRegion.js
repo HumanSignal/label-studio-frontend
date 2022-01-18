1d71554f89a4d2426761a2a7e7f8fd7e8fc96f2c
Max Tkachenko <makseq@gmail.com>
2022-01-18 04:10:46 +0300

    Remove console log

--- src/regions/VideoRectangleRegion.js
+++ src/regions/VideoRectangleRegion.js
@@ -18,8 +18,6 @@ const Model = types
     getShape(frame) {
       let prev, next;
 
-      console.log(self.sequence);
-
       for (const item of self.sequence) {
         if (item.frame === frame) {
           return onlyProps(self.props, item);
