4d4ef88fa2ad47ec5006068642e21ae98afe2518
Koshevarov Sergey <gondragos@gmail.com>
2022-03-10 00:05:09 +0100

    clean: DEV-1494: Remove feature flag

--- src/utils/feature-flags.ts
+++ src/utils/feature-flags.ts
@@ -32,9 +32,6 @@ export const FF_DEV_1372 = "ff_front_dev_1372_visible_when_choice_unselected_110
 // Add an interactivity flag to the results to make some predictions' results be able to be automatically added to newly created annotations.
 export const FF_DEV_1621 = "ff_front_dev_1621_interactive_mode_150222_short";
 
-// Keep enabled state of video region on area transformations
-export const FF_DEV_1494 = "ff_front_dev_1494_keep_enabled_on_update_090322_short";
-
 function getFeatureFlags() {
   return window.APP_SETTINGS?.feature_flags || {};
 }
