54d7b31dbb5539de07128ee89e51e963f5d9fd33
Nick Skriabin <767890+nicholasrq@users.noreply.github.com>
2022-04-06 16:12:59 +0400

    Fix view all for audio and video (#552)

--- src/env/development.js
+++ src/env/development.js
@@ -66,7 +66,7 @@ import { TimeSeriesSingle } from "../examples/timeseries_single";
  */
 // import { AllTypes } from "../examples/all_types";
 
-const data = ImageOCR;
+const data = VideoRectangles;
 
 function getData(task) {
   if (task && task.data) {
