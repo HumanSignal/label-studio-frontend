e9a3190ddcd0da50a43014ba508b16b213fb64fe
Nick Skriabin <767890+nicholasrq@users.noreply.github.com>
2022-05-12 17:15:47 +0400

    fix: DEV-2267: Truncate comments (#609)
    * Properly truncate long comments
    
    * Prevent item selection on collapse/expand
    
    * Comment

--- src/env/development.js
+++ src/env/development.js
@@ -67,7 +67,7 @@ import { TimeSeriesSingle } from "../examples/timeseries_single";
  */
 // import { AllTypes } from "../examples/all_types";
 
-const data = RichTextPlain;
+const data = ImageBbox;
 
 function getData(task) {
   if (task && task.data) {
