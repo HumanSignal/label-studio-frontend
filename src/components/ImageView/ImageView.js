75f21ac213dd1e83efa8d0778153b3a8ff6c2cbf
hlomzik <hlomzik@gmail.com>
2022-05-12 15:40:52 +0400

    fix: DEV-930: canvas should fill all the space (#531)
    * Remove unused initialWidth/initialHeight
    
    * Don't pan with right mouse button, it doesn't work
    
    * Split zoom into stage and image zooming
    
    Stage size affects everything, so no need to scale stuff.
    But when stage is in max size (=container size) we should scale
    image and regions.
    
    * don't keep aspect ration for stage
    
    canvas should cover all possible image space, no cropping.
    fix zoomPositioning to keep image into frame.
    
    * move canvas size calculations to getter
    
    * Also fit rotated image to canvas
    
    * fix nanoid import

--- src/components/ImageView/ImageView.js
+++ src/components/ImageView/ImageView.js
@@ -760,10 +760,7 @@ export default observer(
             />
             <div
               className={styles.frame}
-              style={{
-                width: item.stageComponentSize.width,
-                height: item.stageComponentSize.height,
-              }}
+              style={item.canvasSize}
             >
               <img
                 ref={ref => {
@@ -786,10 +783,10 @@ export default observer(
               }}
               style={{ position: "absolute", top: 0, left: 0 }}
               className={"image-element"}
-              width={item.stageComponentSize.width}
-              height={item.stageComponentSize.height}
-              scaleX={item.stageScale}
-              scaleY={item.stageScale}
+              width={item.canvasSize.width}
+              height={item.canvasSize.height}
+              scaleX={item.zoomScale}
+              scaleY={item.zoomScale}
               x={item.zoomingPositionX}
               y={item.zoomingPositionY}
               offsetX={item.stageTranslate.x}
