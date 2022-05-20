71916cca1854650a4c269f5adc8c49c4cd2460be
Sergey <Gondragos@gmail.com>
2022-05-20 18:49:57 +0200

    fix: DEV-2408: Regions disappearing from UI (#631)
    * fix: DEV-2408: Regions disappearing from UI
    
    * test: DEV-2408: Add test for wrong reordered results of per-region textarea and richtext
    
    (cherry picked from commit 6ef0657ffa22fe4001129a84ef8aa21e430b2a0b)

--- src/stores/Annotation/Annotation.js
+++ src/stores/Annotation/Annotation.js
@@ -14,6 +14,7 @@ import Area from "../../regions/Area";
 import throttle from "lodash.throttle";
 import { UserExtended } from "../UserStore";
 import { FF_DEV_2100, FF_DEV_2100_A, isFF } from "../../utils/feature-flags";
+import Result from "../../regions/Result";
 
 const hotkeys = Hotkey("Annotations", "Annotations");
 
@@ -937,6 +938,15 @@ export const Annotation = types
         const areaId = `${id || guidGenerator()}#${self.id}`;
         const resultId = `${data.from_name}@${areaId}`;
         const value = self.prepareValue(rawValue, tagType);
+        // This should fix a problem when the order of results is broken
+        const omitValueFields = (value) => {
+          const newValue = { ...value };
+
+          Result.properties.value.propertyNames.forEach(propName => {
+            delete newValue[propName];
+          });
+          return newValue;
+        };
 
         let area = getArea(areaId);
 
@@ -945,7 +955,8 @@ export const Annotation = types
             id: areaId,
             object: data.to_name,
             ...data,
-            ...value,
+            // We need to omit value properties due to there may be conflicting property types, for example a text.
+            ...omitValueFields(value),
             value,
           };
 
