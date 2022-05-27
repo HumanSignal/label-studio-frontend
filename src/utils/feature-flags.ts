039f81487fcf7f88ce0e1fab53a87442a5e5434b
hlomzik <hlomzik@gmail.com>
2022-05-27 12:50:14 +0400

    feat: DEV-2458: Add comment on skip (ff)

--- src/utils/feature-flags.ts
+++ src/utils/feature-flags.ts
@@ -57,6 +57,8 @@ export const FF_DEV_2290 = "ff_front_dev_2290_draft_in_annotation_history_short"
 
 export const FF_DEV_2186 = "ff_front_dev_2186_comments_for_update";
 
+export const FF_DEV_2458 = "ff_front_dev_2458_comments_for_skip_250522_short";
+
 function getFeatureFlags() {
   return window.APP_SETTINGS?.feature_flags || {
     // ff_front_DEV_1713_audio_ui_150222_short: true,
