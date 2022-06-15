c36dec7c872b3c8c23dfe4f02004ffa6e33607af
bmartel <brandonmartel@gmail.com>
2022-06-15 08:47:31 -0500

    fix: DEV-1442: deselection of ImageView bounding box before creating another (#684)
    * fix: DEV-1422: clicking off a previously selected image rect region should deselect and not start a new region
    
    * fix: DEV-1442: image transformer test should deselect before region multiselect
    
    * fix: DEV-1442: travel through brush history test
    
    * fix: DEV-1442: spacing
    
    * fix: DEV-1442: ctrl usage to CommandOrControl in tests
    
    * fix: DEV-1442: put change behind FF
    
    * fix: DEV-1422: update to fix handling to include multi point polyregions

--- src/utils/feature-flags.ts
+++ src/utils/feature-flags.ts
@@ -68,6 +68,8 @@ export const FF_DEV_2458 = "ff_front_dev_2458_comments_for_skip_250522_short";
 
 export const FF_DEV_2480 = "ff_dev_2480_convenient_offsets_270522_short";
 
+export const FF_DEV_1442 = "ff_front_dev_1422_unselect_shape_on_click_outside_080622_short";
+
 export const FF_DEV_2431 = "ff_front_dev_2431_delete_polygon_points_080622_short";
 
 function getFeatureFlags() {
