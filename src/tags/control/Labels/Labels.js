9863f5a56f77a6c063b7702a134a9e0c5e86ef7c
hlomzik <hlomzik@gmail.com>
2022-06-17 00:59:40 +0100

    Fix documentation for Labels
    examples, description, param

--- src/tags/control/Labels/Labels.js
+++ src/tags/control/Labels/Labels.js
@@ -19,9 +19,11 @@ import DynamicChildrenMixin from "../../../mixins/DynamicChildrenMixin";
 import { FF_DEV_2007_DEV_2008, isFF } from "../../../utils/feature-flags";
 
 /**
- * Use the Labels tag to create a set of labels that can be assigned to identified regions. Use with the Label tag to specify the values of labels to assign to regions.
+ * The Labels tag provides a set of labels for labeling regions in tasks for machine learning and data science projects. Use the Labels tag to create a set of labels that can be assigned to identified region and specify the values of labels to assign to regions.
  *
- * Use with the following data types: audio, text. Other data types have type-specific Labels tags.
+ * All types of Labels can have dynamic value to load labels from task. This task data should contain a list of options to create underlying <Label>s. All the parameters from options will be transferred to corresponding tags.
+ *
+ * The Labels tag can be used with audio and text data types. Other data types have type-specific Labels tags.
  * @example
  * <!--Basic labeling configuration to apply labels to a passage of text -->
  * <View>
@@ -32,16 +34,11 @@ import { FF_DEV_2007_DEV_2008, isFF } from "../../../utils/feature-flags";
  *   <Text name="txt-1" value="$text" />
  * </View>
  *
- * @example <caption>This config with dynamic labels</caption>
- * <!--
- * All types of `Labels` can have dynamic `value` to load labels from task.
- * This task data should contain a list of options to create underlying `<Label>`s.
- * All the parameters from options will be transferred to corresponding tags.
- * -->
- * <PolygonLabels name="product" toName="shelf" value="$labels" />
+ * @example <caption>This part of config with dynamic labels</caption>
+ * <Labels name="product" toName="shelf" value="$brands" />
  * <!-- {
  *   "data": {
- *     "options": [
+ *     "brands": [
  *       { "value": "Big brand" },
  *       { "value": "Another brand", "background": "orange" },
  *       { "value": "Local brand" },
@@ -50,12 +47,12 @@ import { FF_DEV_2007_DEV_2008, isFF } from "../../../utils/feature-flags";
  *   }
  * } -->
  * @example <caption>is equivalent to this config</caption>
- * <PolygonLabels name="product" toName="shelf">
+ * <Labels name="product" toName="shelf">
  *   <Label value="Big brand" />
  *   <Label value="Another brand" background="orange" />
  *   <Label value="Local brand" />
  *   <Label value="Green brand" alias="Eco" showAlias="true" />
- * </PolygonLabels>
+ * </Labels>
  * @name Labels
  * @meta_title Labels Tag for Labeling Regions
  * @meta_description Customize Label Studio by using the Labels tag to provide a set of labels for labeling regions in tasks for machine learning and data science projects.
@@ -68,6 +65,7 @@ import { FF_DEV_2007_DEV_2008, isFF } from "../../../utils/feature-flags";
  * @param {string=} [fillColor]              - Rectangle fill color in hexadecimal
  * @param {string=} [strokeColor=#f48a42]    - Stroke color in hexadecimal
  * @param {number=} [strokeWidth=1]          - Width of the stroke
+ * @param {string} [value]                   - Task data field containing a list of dynamically loaded labels (see example below)
  */
 const TagAttrs = types.model({
   name: types.identifier,
