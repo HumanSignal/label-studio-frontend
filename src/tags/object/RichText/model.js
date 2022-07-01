ab38ac6902c8893dd7e6d6e6683478f583f51059
hlomzik <hlomzik@gmail.com>
2022-07-01 17:44:40 +0100

    fix: DEV-2777: remove lookbehind in regex for Safari (#730)

--- src/tags/object/RichText/model.js
+++ src/tags/object/RichText/model.js
@@ -187,7 +187,7 @@ const Model = types
           })
           .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script\s*>/gi, "<ls-stub></ls-stub>")
           .replace(/<iframe\b.*?(?:\/>|<\/iframe>)/g, "<ls-stub></ls-stub>")
-          .replace(/(<?)(?<=\s)on[a-z]+\s*=\s*(?:(['"])(?!\2).+?\2|(?:\S+?\(.*?\)(?=[\s>])))(.*?)/gi, "");
+          .replace(/\bon[a-z]+\s*=\s*(?:(['"])(?!\1).+?\1|(?:\S+?\(.*?\)(?=[\s>])))(.*?)/gi, "");
 
         self._value = val;
 
