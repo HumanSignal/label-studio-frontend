9761e4f485073b89bd90c7205cf15ae032724d8d
Yousif Yassi <yousif@heartex.com>
2022-07-21 12:22:10 -0400

    refactoring the code to make sure the FF is used properly

--- src/components/Entities/LabelList.js
+++ src/components/Entities/LabelList.js
@@ -5,31 +5,12 @@ import { RegionItem } from "./RegionItem";
 import { observer } from "mobx-react";
 import { useState } from "react";
 import { LsChevron } from "../../assets/icons";
+import { FF_DEV_2755, isFF } from "../../utils/feature-flags";
 
 const { localStorage } = window;
 const localStoreName = `collapsed-label-pos`;
 
 export const LabelList = observer(({ regionStore }) => {
-  const [collapsedPos, setCollapsedPos] = useState( localStorage.getItem( localStoreName )?.split?.(",")?.filter( pos => !!pos ) ?? [] );
-
-  const updateLocalStorage = ( collapsedPos ) => {
-    localStorage.setItem( localStoreName, collapsedPos );
-  };
-
-  const collapse = ( pos ) => {
-    const newCollapsedPos = [...collapsedPos, pos];
-
-    setCollapsedPos( newCollapsedPos );
-    updateLocalStorage( newCollapsedPos );
-  };
-
-  const expand = ( pos ) => {
-    const newCollapsedPos = collapsedPos.filter( cPos => cPos !== pos );
-    
-    setCollapsedPos( newCollapsedPos );
-    updateLocalStorage( newCollapsedPos );
-  };
-
   const treeData = regionStore.asLabelsTree((item, idx, isLabel, children, onClick) => {
     return {
       key: item.id,
@@ -43,7 +24,47 @@ export const LabelList = observer(({ regionStore }) => {
     };
   });
 
-  const expandedKeys = treeData.filter( item => !collapsedPos.includes( item.pos ) ).map( item => item.key ) ?? [];
+  if( isFF(FF_DEV_2755) ) {
+    const [collapsedPos, setCollapsedPos] = useState( localStorage.getItem( localStoreName )?.split?.(",")?.filter( pos => !!pos ) ?? [] );
+  
+    const updateLocalStorage = ( collapsedPos ) => {
+      localStorage.setItem( localStoreName, collapsedPos );
+    };
+  
+    const collapse = ( pos ) => {
+      const newCollapsedPos = [...collapsedPos, pos];
+  
+      setCollapsedPos( newCollapsedPos );
+      updateLocalStorage( newCollapsedPos );
+    };
+  
+    const expand = ( pos ) => {
+      const newCollapsedPos = collapsedPos.filter( cPos => cPos !== pos );
+      
+      setCollapsedPos( newCollapsedPos );
+      updateLocalStorage( newCollapsedPos );
+    };
+    const expandedKeys = treeData.filter( item => !collapsedPos.includes( item.pos ) ).map( item => item.key ) ?? [];
+  
+    return (
+      <Tree
+        className={styles.treelabels}
+        treeData={treeData}
+        showIcon={false}
+        blockNode={true}
+        defaultExpandAll={true}
+        autoExpandParent={true}
+        expandedKeys={expandedKeys}
+        switcherIcon={<LsChevron className={styles.switcherIcon} opacity="0.25" />}
+        onExpand={( internalExpandedKeys, { node } ) => {
+          const nodeRegion = treeData.find(region => region.key === node.key);
+          const pos = nodeRegion?.pos;
+  
+          collapsedPos.includes(pos) ? expand(pos) : collapse(pos);
+        }}
+      />
+    );
+  }
 
   return (
     <Tree
@@ -53,14 +74,7 @@ export const LabelList = observer(({ regionStore }) => {
       blockNode={true}
       defaultExpandAll={true}
       autoExpandParent={true}
-      expandedKeys={expandedKeys}
-      switcherIcon={<LsChevron className={styles.switcherIcon} opacity="0.25" />}
-      onExpand={( internalExpandedKeys, { node } ) => {
-        const nodeRegion = treeData.find(region => region.key === node.key);
-        const pos = nodeRegion?.pos;
-
-        collapsedPos.includes(pos) ? expand(pos) : collapse(pos);
-      }}
+      switcherIcon={<LsChevron opacity="0.25" />}
     />
   );
 });
