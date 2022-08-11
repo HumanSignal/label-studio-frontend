b0e87f7a9f5dda32ef86ceeb904f0bec7aa0876b
Nick Skriabin <nr@fenelon.ru>
2022-08-11 22:51:00 +0400

    Fix showing annotation history

--- src/components/CurrentEntity/CurrentEntity.js
+++ src/components/CurrentEntity/CurrentEntity.js
@@ -117,7 +117,7 @@ export const CurrentEntity = injector(observer(({
         </Elem>
       )}
       <AnnotationHistory
-        showHistory={showHistory}
+        enabled={showHistory}
         showDraft={showDraftInHistory}
       />
     </Block>
