8cbee6fdd0109b0d43c1c621d74c10ea16ee2270
Sergey <sergey.koshevarov@heartex.com>
2022-08-22 18:07:50 +0200

    fix: DEV-2968: Video regions history

--- src/regions/VideoRegion.js
+++ src/regions/VideoRegion.js
@@ -30,7 +30,7 @@ const Model = types
     sequence: types.frozen([]),
   })
   .preProcessSnapshot((snapshot) => {
-    return { ...snapshot, sequence: snapshot.value.sequence };
+    return { ...snapshot, sequence: snapshot.sequence || snapshot.value.sequence };
   })
   .volatile(() => ({
     hideable: true,
