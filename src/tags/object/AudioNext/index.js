84d6e56d4bb9824571a6c2a35e8209363a4d0529
Julio Sgarbi <julio.sgarbi@hotmail.com>
2022-09-14 11:35:02 -0300

    override next with ultra

--- src/tags/object/AudioNext/index.js
+++ src/tags/object/AudioNext/index.js
@@ -1,30 +1,24 @@
 import Registry from "../../../core/Registry";
 import { AudioModel } from "./model";
-import { AudioUltraModel } from "../AudioUltra/model";
 import { HtxAudio } from "./view_old";
 import { AudioNext } from './view';
-import { AudioView } from '../AudioUltra/view';
 import { AudioRegionModel } from "../../../regions/AudioRegion";
 import { FF_DEV_1713, FF_DEV_2715, isFF } from "../../../utils/feature-flags";
 
 // Fallback to the previos version
-let _tagView = HtxAudio;
-let _model = AudioModel;
+let TagView = HtxAudio;
 
 if (isFF(FF_DEV_1713)) {
-  _tagView = AudioNext;
+  TagView = AudioNext;
 }
-
-if (isFF(FF_DEV_2715)) {
-  _model = AudioUltraModel;
-  _tagView = AudioView;
-}
-
 
 // Replacing both Audio and AudioPlus
 // Must add a deprecation warning
-Registry.addTag("audio", _model, _tagView);
-Registry.addTag("audioplus", _model, _tagView);
-Registry.addObjectType(_model);
+if (!isFF(FF_DEV_2715)) {
+  Registry.addTag("audio", AudioModel, TagView);
+  Registry.addTag("audioplus", AudioModel, TagView);
+}
 
-export { AudioRegionModel, _model as AudioModel, HtxAudio };
+Registry.addObjectType(AudioModel);
+
+export { AudioRegionModel, AudioModel, HtxAudio };
--- src/tags/object/AudioUltra/index.js
+++ src/tags/object/AudioUltra/index.js
@@ -1,9 +1,13 @@
 import Registry from "../../../core/Registry";
 import { AudioUltraModel } from "./model";
 import { AudioView } from './view';
+import { FF_DEV_2715, isFF } from "../../../utils/feature-flags";
 
+if (isFF(FF_DEV_2715)) {
+  Registry.addTag("audio", AudioUltraModel, AudioView);
+  Registry.addTag("audioplus", AudioUltraModel, AudioView);
+}
 
-Registry.addTag("audioultra", AudioUltraModel, AudioView);
 Registry.addObjectType(AudioUltraModel);
 
 export { AudioView, AudioUltraModel };
