54e1bb1d3b3a03355f4f99a6a48d9baa9249d754
Brandon Martel <brandonmartel@gmail.com>
2022-09-15 12:20:42 -0500

    wire up the new audio lib

--- src/tags/object/AudioUltra/view.tsx
+++ src/tags/object/AudioUltra/view.tsx
@@ -1,11 +1,29 @@
 import { observer } from "mobx-react";
-import { FC, useCallback } from "react";
+import { FC, useCallback, useRef } from "react";
+import { useWaveform } from "../../../lib/AudioUltra/react";
 
 interface AudioUltraProps {
   item: any;
 }
 
-const AudioUltraView: FC<AudioUltraProps> = () => {
+const AudioUltraView: FC<AudioUltraProps> = ({ item }) => {
+  const rootRef = useRef<HTMLElement | null>();
+
+  // const { waveform: _, ...controls } =
+  useWaveform(rootRef, {
+    src: item._value,
+    waveColor: "#BEB9C5",
+    gridColor: "#BEB9C5",
+    gridWidth: 1,
+    backgroundColor: "#ffffff",
+    autoCenter: true,
+    zoomToCursor: true,
+    enabledChannels: [0],
+    height: 94,
+    zoom: 1,
+    muted: false,
+    rate: 1,
+  });
 
   const handleReady = useCallback(() => {
   }, []);
@@ -29,7 +47,7 @@ const AudioUltraView: FC<AudioUltraProps> = () => {
   }, []);
 
   return (
-    <>AudioUltra</>
+    <div ref={(el) => (rootRef.current = el)}></div>
   );
 };
 
