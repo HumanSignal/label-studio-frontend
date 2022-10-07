7efe2e78298fa6180321a9954035b9dbe7b8a65c
Yousif Yassi <yousif@heartex.com>
2022-10-07 11:06:03 -0400

    tweaking the new taxonomy e2e test

--- e2e/tests/taxonomy.test.js
+++ e2e/tests/taxonomy.test.js
@@ -25,47 +25,74 @@ const config = `<View>
   </Taxonomy>
 </View>
 `;
-
-
-
+const configShowFullPath = `<View>
+  <Text name="text" value="$text"/>
+  <Labels name="label" toName="text">
+    <Label value="PER" background="red"/>
+    <Label value="ORG" background="darkorange"/>
+    <Label value="LOC" background="orange"/>
+    <Label value="MISC" background="green"/>
+  </Labels>
+  <Taxonomy name="taxonomy" showFullPath="true" toName="text" perRegion="true" >
+    <Choice value="Archaea" />
+    <Choice value="Bacteria" />
+    <Choice value="Eukarya">
+      <Choice value="Human" />
+      <Choice value="Oppossum" />
+      <Choice value="Extraterrestial" selected="true" />
+    </Choice>
+  </Taxonomy>
+</View>
+`;
 const data = {
   text: `To have faith is to trust yourself to the water`,
 };
-
 const annotations = [
-  { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To' },
+  { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To', taxonmy: ["Eukarya", "Extraterrestial"], taxonomyLabel: "Extraterrestial" },
+  { label: 'PER', rangeStart: 3, rangeEnd: 7, text: 'have', taxonmy: ["Archaea"], taxonomyLabel: "Archaea" },
+];
+const annotationsShowFullPath = [
+  { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To', taxonmy: ["Eukarya", "Extraterrestial"], taxonomyLabel: "Eukarya / Extraterrestial" },
+];
+const tests = [
+  { title: "Check Taxonomy", params: { config, data }, annotations },
+  // { title: "Check Taxonomy with showFullPath", params: { configShowFullPath, data }, annotations: annotationsShowFullPath }
 ];
 
-const params = { config, data };
+tests.forEach(t => {
+  const { title, annotations, params } = t;
 
-Scenario("Check Taxonomy holds state between annotations and saves result", async function({ I, LabelStudio }) {
+  Scenario(title, async ({ I, LabelStudio }) => {
 
-  I.amOnPage("/");
-
-  LabelStudio.init(params);
-
-  annotations.forEach(annotation => {
-    I.click(locate("span").withText(annotation.label));
-    I.executeScript(selectText, {
-      selector: ".lsf-htx-richtext",
-      rangeStart: annotation.rangeStart,
-      rangeEnd: annotation.rangeEnd,
+    I.amOnPage("/");
+  
+    LabelStudio.init(params);
+  
+    annotations.forEach(annotation => {
+      I.click(locate("span").withText(annotation.label));
+      I.executeScript(selectText, {
+        selector: ".lsf-htx-richtext",
+        rangeStart: annotation.rangeStart,
+        rangeEnd: annotation.rangeEnd,
+      });
+      I.click(locate("span").withText(annotation.label));
+      I.click(locate("li").withText(annotation.text));
     });
-    I.click(locate("span").withText(annotation.label));
-    I.click(locate("li").withText(annotation.text));
+
+    annotations.forEach(annotation => {
+      I.click(locate("li").withText(annotation.text));
+    });
+
+    const results = await I.executeScript(serialize);
+  
+    results.filter(result => result.value.labels).forEach((result, index) => {
+      const input = annotations[index];
+      const expected = { end: input.rangeEnd, labels: [input.label], start: input.rangeStart, text: input.text };
+      
+      assert.deepEqual(result.value, expected);
+
+      locate("div").withText(input.taxonomyLabel);
+    });
+  
   });
-
-  annotations.forEach(annotation => {
-    I.click(locate("li").withText(annotation.text));
-
-  });
-  const results = await I.executeScript(serialize);
-
-  results.filter(result => result.value.labels).forEach((result, index) => {
-    const input = annotations[index];
-    const expected = { end: input.rangeEnd, labels: [input.label], start: input.rangeStart, text: input.text };
-    
-    assert.deepEqual(result.value, expected);
-  });
-
 });
