3780da765a2afdcc53b6f2071772f3fb77e9443b
Yousif Yassi <yousif@heartex.com>
2022-10-11 16:59:50 +0100

    updating e2e test to use codeceptjs Data

--- e2e/tests/taxonomy.test.js
+++ e2e/tests/taxonomy.test.js
@@ -1,4 +1,4 @@
-/* global Feature, Scenario, locate */
+/* global Feature, Scenario, locate, DataTable, Data */
 
 const { serialize, selectText } = require("./helpers");
 
@@ -6,93 +6,97 @@ const assert = require("assert");
 
 Feature("Taxonomy");
 
-const config = `<View>
-  <Text name="text" value="$text"/>
-  <Labels name="label" toName="text">
-    <Label value="PER" background="red"/>
-    <Label value="ORG" background="darkorange"/>
-    <Label value="LOC" background="orange"/>
-    <Label value="MISC" background="green"/>
-  </Labels>
-  <Taxonomy name="taxonomy" toName="text" perRegion="true" >
-    <Choice value="Archaea" />
-    <Choice value="Bacteria" />
-    <Choice value="Eukarya">
-      <Choice value="Human" />
-      <Choice value="Oppossum" />
-      <Choice value="Extraterrestial" selected="true" />
-    </Choice>
-  </Taxonomy>
-</View>
-`;
-const configShowFullPath = `<View>
-  <Text name="text" value="$text"/>
-  <Labels name="label" toName="text">
-    <Label value="PER" background="red"/>
-    <Label value="ORG" background="darkorange"/>
-    <Label value="LOC" background="orange"/>
-    <Label value="MISC" background="green"/>
-  </Labels>
-  <Taxonomy name="taxonomy" showfullpath="true" toName="text" perRegion="true" >
-    <Choice value="Archaea" />
-    <Choice value="Bacteria" />
-    <Choice value="Eukarya">
-      <Choice value="Human" />
-      <Choice value="Oppossum" />
-      <Choice value="Extraterrestial" selected="true" />
-    </Choice>
-  </Taxonomy>
-</View>
-`;
-const data = {
-  text: `To have faith is to trust yourself to the water`,
+const cases = {
+  taxonmy: {
+    config: `<View>
+      <Text name="text" value="$text"/>
+      <Labels name="label" toName="text">
+        <Label value="PER" background="red"/>
+        <Label value="ORG" background="darkorange"/>
+        <Label value="LOC" background="orange"/>
+        <Label value="MISC" background="green"/>
+      </Labels>
+      <Taxonomy name="taxonomy" toName="text" perRegion="true" >
+        <Choice value="Archaea" />
+        <Choice value="Bacteria" />
+        <Choice value="Eukarya">
+          <Choice value="Human" />
+          <Choice value="Oppossum" />
+          <Choice value="Extraterrestial" selected="true" />
+        </Choice>
+      </Taxonomy>
+    </View>`,
+    text: `To have faith is to trust yourself to the water`,
+    annotations: [
+      { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To', taxonmy: ["Eukarya", "Extraterrestial"], taxonomyLabel: "Extraterrestial" },
+      { label: 'PER', rangeStart: 3, rangeEnd: 7, text: 'have', taxonmy: ["Archaea"], taxonomyLabel: "Archaea" },
+    ],
+  },
+  taxonomyWithShowLabels: {
+    config: `<View>
+      <Text name="text" value="$text"/>
+      <Labels name="label" toName="text">
+        <Label value="PER" background="red"/>
+        <Label value="ORG" background="darkorange"/>
+        <Label value="LOC" background="orange"/>
+        <Label value="MISC" background="green"/>
+      </Labels>
+      <Taxonomy name="taxonomy" showfullpath="true" toName="text" perRegion="true" >
+        <Choice value="Archaea" />
+        <Choice value="Bacteria" />
+        <Choice value="Eukarya">
+          <Choice value="Human" />
+          <Choice value="Oppossum" />
+          <Choice value="Extraterrestial" selected="true" />
+        </Choice>
+      </Taxonomy>
+    </View>`,
+    text: `To have faith is to trust yourself to the water`,
+    annotations: [
+      { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To', taxonmy: ["Eukarya", "Extraterrestial"], taxonomyLabel: "Eukarya / Extraterrestial" },
+    ],
+  },
 };
-const annotations = [
-  { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To', taxonmy: ["Eukarya", "Extraterrestial"], taxonomyLabel: "Extraterrestial" },
-  { label: 'PER', rangeStart: 3, rangeEnd: 7, text: 'have', taxonmy: ["Archaea"], taxonomyLabel: "Archaea" },
-];
-const annotationsShowFullPath = [
-  { label: 'PER', rangeStart: 0, rangeEnd: 2, text: 'To', taxonmy: ["Eukarya", "Extraterrestial"], taxonomyLabel: "Eukarya / Extraterrestial" },
-];
-const tests = [
-  { title: "Check Taxonomy", params: { config, data }, annotations },
-  { title: "Check Taxonomy with showFullPath", params: { config: configShowFullPath, data }, annotations: annotationsShowFullPath },
-];
 
-tests.forEach(t => {
-  const { title, annotations, params } = t;
+const taxonomyTable = new DataTable(["taxnomyName"]);
 
-  Scenario(title, async ({ I, LabelStudio }) => {
+for (const taxonomyName of Object.keys(cases)) {
+  taxonomyTable.add([taxonomyName]);
+}
 
-    I.amOnPage("/");
-  
-    LabelStudio.init(params);
-  
-    annotations.forEach(annotation => {
-      I.click(locate("span").withText(annotation.label));
-      I.executeScript(selectText, {
-        selector: ".lsf-htx-richtext",
-        rangeStart: annotation.rangeStart,
-        rangeEnd: annotation.rangeEnd,
-      });
-      I.click(locate("span").withText(annotation.label));
-      I.click(locate("li").withText(annotation.text));
+Data(taxonomyTable).Scenario("Check Taxonomy", async ({ I, LabelStudio, current }) => {
+  const { taxnomyName } = current;
+  const Taxonomy = cases[taxnomyName];
+  const { annotations, config, text } = Taxonomy;
+
+  I.amOnPage("/");
+
+  LabelStudio.init({ config, data: { text } });
+
+  annotations.forEach(annotation => {
+    I.click(locate("span").withText(annotation.label));
+    I.executeScript(selectText, {
+      selector: ".lsf-htx-richtext",
+      rangeStart: annotation.rangeStart,
+      rangeEnd: annotation.rangeEnd,
     });
-
-    annotations.forEach(annotation => {
-      I.click(locate("li").withText(annotation.text));
-    });
-
-    const results = await I.executeScript(serialize);
-  
-    results.filter(result => result.value.labels).forEach((result, index) => {
-      const input = annotations[index];
-      const expected = { end: input.rangeEnd, labels: [input.label], start: input.rangeStart, text: input.text };
-      
-      assert.deepEqual(result.value, expected);
-
-      locate("div").withText(input.taxonomyLabel);
-    });
-  
+    I.click(locate("span").withText(annotation.label));
+    I.click(locate("li").withText(annotation.text));
   });
+
+  annotations.forEach(annotation => {
+    I.click(locate("li").withText(annotation.text));
+  });
+
+  const results = await I.executeScript(serialize);
+
+  results.filter(result => result.value.labels).forEach((result, index) => {
+    const annotation = annotations[index];
+    const expected = { end: annotation.rangeEnd, labels: [annotation.label], start: annotation.rangeStart, text: annotation.text };
+    
+    assert.deepEqual(result.value, expected);
+
+    locate("div").withText(annotation.taxonomyLabel);
+  });
+  
 });
