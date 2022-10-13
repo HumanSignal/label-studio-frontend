9c4b69e10e1403b5551fefe97238ffbd81e5e23e
Julio Sgarbi <julio.sgarbi@hotmail.com>
2022-10-13 12:51:28 +0100

    fix: DEV-3559: Taxonomy annotations are not displayed after selection (#903)
    * fix: DEV-3559: Taxonomy annotations are not displayed after selection in paginated documents
    
    * remove lazyload
    
    * fix: DEV-3559: Retain region selection during Image resize history reinit update callbacks
    
    Co-authored-by: Brandon Martel <brandonmartel@gmail.com>

--- src/tags/object/Image.js
+++ src/tags/object/Image.js
@@ -899,7 +899,8 @@ const Model = types.model({
 
       //sometimes when user zoomed in, annotation was creating a new history. This fix that in case the user has nothing in the history yet
       if (_historyLength <= 1){
-        setTimeout(self.annotation.reinitHistory, 0);
+        // Don't force unselection of regions during the updateObjects callback from history reinit
+        setTimeout(() => self.annotation.reinitHistory(false), 0);
       }
     },
 
@@ -920,7 +921,8 @@ const Model = types.model({
       } else {
         self.sizeToAuto();
       }
-      setTimeout(self.annotation.reinitHistory, 0);
+      // Don't force unselection of regions during the updateObjects callback from history reinit
+      setTimeout(() => self.annotation.reinitHistory(false), 0);
     },
 
     checkLabels() {
