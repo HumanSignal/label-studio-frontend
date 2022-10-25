1358b242261ff56531e52d9edd4ed74fc28bd447
hlomzik <hlomzik@gmail.com>
2022-10-25 14:21:10 +0100

    Finally use parent Annotation, not selected

--- src/mixins/AnnotationMixin.js
+++ src/mixins/AnnotationMixin.js
@@ -1,12 +1,11 @@
 import { getRoot, types } from "mobx-state-tree";
+import Types from "../core/Types";
 
 export const AnnotationMixin = types.model("AnnotationMixin", {
 
 }).views((self) => ({
   get annotation() {
-    const as = self.annotationStore;
-
-    return as?.selectedHistory ?? as?.selected;
+    return Types.getParentOfTypeString(self, "Annotation");
   },
 
   get annotationStore() {
