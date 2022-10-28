2553bcb48c4e84b48d656d5fc7db5c1f3881271f
Julio Sgarbi <julio.sgarbi@hotmail.com>
2022-10-28 15:48:24 +0200

    remove alert

--- src/components/HtxTextBox/HtxTextBox.js
+++ src/components/HtxTextBox/HtxTextBox.js
@@ -80,7 +80,6 @@ export class HtxTextBox extends React.Component {
   };
 
   save = () => {
-    alert('save ' + this.state.value);
     this.props.onChange(this.state.value);
     this.setEditing(false);
   };
@@ -106,12 +105,10 @@ export class HtxTextBox extends React.Component {
       ref: this.inputRef,
       value,
       onBlur: isFF(FF_DEV_1566) ? ()=>{
-        alert('change '+ this.state.value);
         onChange(this.state.value);
       } : this.save,
       onFocus,
       onChange: e => {
-        alert(e.target.value);
         this.setValue(e.target.value);
         this.updateHeight();
       },
