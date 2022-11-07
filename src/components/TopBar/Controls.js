f9afea343f2dd4fb45b3e11f6afd36180044888f
Yousif Yassi <yousif@heartex.com>
2022-11-07 11:39:43 -0500

    simplifying the expression since if any are true we should toggle disable

--- src/components/TopBar/Controls.js
+++ src/components/TopBar/Controls.js
@@ -39,7 +39,7 @@ export const Controls = controlsInjector(observer(({ store, history, annotation 
   const [isInProgress, setIsInProgress] = useState(false);
 
   // const isReady = store.annotationStore.selected.objects.every(object => object.isReady === undefined || object.isReady);
-  const disabled = (store.isSubmitting || historySelected) && isInProgress; // || !isReady;
+  const disabled = store.isSubmitting || historySelected || isInProgress; // || !isReady;
   const submitDisabled = store.hasInterface("annotations:deny-empty") && results.length === 0;
   
   const buttonHandler = useCallback(async (e, callback, tooltipMessage) => {
