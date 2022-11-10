a517454dc61b8b6072dcbf238744dc349ad73f9c
Sergey <sergey.koshevarov@heartex.com>
2022-11-10 06:42:19 +0100

    fix: DEV-1852: Fix missing labels on video regions

--- src/tags/object/Video/Rectangle.js
+++ src/tags/object/Video/Rectangle.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Rect } from "react-konva";
+import React, { useMemo } from "react";
+import { Group, Rect } from "react-konva";
 import { observer } from "mobx-react";
 import { useRegionStyles } from "../../../hooks/useRegionColor";
+import { LabelOnVideoBbox } from "../../../components/ImageView/LabelOnRegion";
 
 const getNodeAbsoluteDimensions = (node, workingArea) => {
   const { realWidth: width, realHeight: height } = workingArea;
@@ -21,46 +22,56 @@ const RectanglePure = ({ reg, frame, workingArea, ...rest }) => {
 
   if (!box) return null;
 
-  const { realWidth: waWidth, realHeight: waHeight } = workingArea;
+  const { realWidth: waWidth, realHeight: waHeight, scale: waScale } = workingArea;
 
-  const newBox = {
+  const newBox = useMemo(()=>({
     x: box.x * waWidth / 100,
     y: box.y * waHeight / 100,
     width: box.width * waWidth / 100,
     height: box.height * waHeight / 100,
     rotation: box.rotation,
-  };
+  }), [box, waWidth, waHeight]);
 
   return reg.isInLifespan(frame) ? (
-    <Rect
-      {...newBox}
-      draggable
-      fill={style.fillColor}
-      stroke={style.strokeColor}
-      strokeScaleEnabled={false}
-      opacity={reg.hidden ? 0 : 1}
-      onTransformEnd={e => {
-        const node = e.target;
-        const scaleX = node.scaleX();
-        const scaleY = node.scaleY();
-        // set minimal value
-        const w = Math.max(3, node.width() * scaleX);
-        const h = Math.max(3, node.height() * scaleY);
+    <Group>
+      <LabelOnVideoBbox
+        reg={reg}
+        box={newBox}
+        scale={waScale}
+        color={style.strokeColor}
+        strokeWidth={style.strokeWidth}
+        adjacent
+      />
+      <Rect
+        {...newBox}
+        draggable
+        fill={style.fillColor}
+        stroke={style.strokeColor}
+        strokeScaleEnabled={false}
+        opacity={reg.hidden ? 0 : 1}
+        onTransformEnd={e => {
+          const node = e.target;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+          // set minimal value
+          const w = Math.max(3, node.width() * scaleX);
+          const h = Math.max(3, node.height() * scaleY);
 
-        node.scaleX(1);
-        node.scaleY(1);
-        node.setWidth(w);
-        node.setHeight(h);
+          node.scaleX(1);
+          node.scaleY(1);
+          node.setWidth(w);
+          node.setHeight(h);
 
-        reg.updateShape(getNodeAbsoluteDimensions(node, workingArea), frame);
-      }}
-      onDragEnd={e => {
-        const node = e.target;
+          reg.updateShape(getNodeAbsoluteDimensions(node, workingArea), frame);
+        }}
+        onDragEnd={e => {
+          const node = e.target;
 
-        reg.updateShape(getNodeAbsoluteDimensions(node, workingArea), frame);
-      }}
-      {...rest}
-    />
+          reg.updateShape(getNodeAbsoluteDimensions(node, workingArea), frame);
+        }}
+        {...rest}
+      />
+    </Group>
   ) : null;
 };
 
