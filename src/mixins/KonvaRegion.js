2bd27fc2ea44742c0136479d2bcdbb6ae4147b27
Yousif Yassi <yousif@heartex.com>
2022-11-17 11:11:21 -0500

    minor clean up

--- src/mixins/KonvaRegion.js
+++ src/mixins/KonvaRegion.js
@@ -36,7 +36,6 @@ export const KonvaRegionMixin = types.model({})
         const ev = e?.evt || e;
         const additiveMode = ev?.ctrlKey || ev?.metaKey;
 
-        
         if (e) e.cancelBubble = true;
 
         if (annotation.editable && annotation.relationMode) {
