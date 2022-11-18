a808ab3f46e55a3bb2167d9685aef5ddd6518a66
Travis Clark <travisjosephclark@gmail.com>
2022-11-18 13:56:12 -0600

    remove unused

--- src/components/Comments/CommentItem.tsx
+++ src/components/Comments/CommentItem.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react";
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { Tooltip } from "antd";
 import { IconCheck, IconEllipsis } from "../../assets/icons";
 import { Space } from "../../common/Space/Space";
