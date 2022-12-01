a134e9f5242b46b6cc567adfad0e542775a4cc90
Brandon Martel <brandonmartel@gmail.com>
2022-12-01 10:16:34 -0600

    put video + pargraph sync under same existing FF for audio + paragraph sync

--- src/tags/object/Paragraphs/Model.js
+++ src/tags/object/Paragraphs/Model.js
@@ -13,12 +13,11 @@ import styles from "./Paragraphs.module.scss";
 import { errorBuilder } from "../../../core/DataValidator/ConfigValidator";
 import { AnnotationMixin } from "../../../mixins/AnnotationMixin";
 import { isValidObjectURL } from "../../../utils/utilities";
-import { FF_DEV_2461, FF_DEV_2669, FF_DEV_2715, FF_DEV_2918, isFF } from "../../../utils/feature-flags";
+import { FF_DEV_2461, FF_DEV_2669, FF_DEV_2918, isFF } from "../../../utils/feature-flags";
 import { SyncMixin } from "../../../mixins/SyncMixin";
 
 
 const isFFDev2461 = isFF(FF_DEV_2461);
-const isFFDev2715 = isFF(FF_DEV_2715);
 
 /**
  * The `Paragraphs` tag displays paragraphs of text on the labeling interface. Use to label dialogue transcripts for NLP and NER projects.
@@ -87,11 +86,9 @@ const Model = types
     },
 
     get syncedAudioVideo() {
-      if (!isFFDev2461 && !isFFDev2715) return false;
-      if (isFFDev2715) {
-        return self.syncedObject?.type?.startsWith("audio") || self.syncedObject?.type?.startsWith("video");
-      }
-      return self.syncedObject?.type?.startsWith("audio");
+      if (!isFFDev2461) return false;
+
+      return self.syncedObject?.type?.startsWith("audio") || self.syncedObject?.type?.startsWith("video");
     },
 
     get audio() {
