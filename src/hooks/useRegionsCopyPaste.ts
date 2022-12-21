cbecf521136a74416f8de494dd2e20271c197b8a
Nick Skriabin <nr@fenelon.ru>
2022-12-21 17:30:13 +0000

    Fix copying NER regions

--- src/hooks/useRegionsCopyPaste.ts
+++ src/hooks/useRegionsCopyPaste.ts
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 export const useRegionsCopyPaste = (entity: any) => {
   useEffect(()=>{
     const isFocusable = (el: Node | Window | HTMLElement | null) => {
+      if (!el) return false;
       if ((el as Node).nodeType !== Node.ELEMENT_NODE) return false;
 
       const element = el as HTMLElement;
@@ -26,11 +27,14 @@ export const useRegionsCopyPaste = (entity: any) => {
       const data = clipboardData?.getData('application/json');
 
       try {
-        const results = data ? JSON.parse(data) : {};
+        const results = (data ? JSON.parse(data) : []).map((res: any) => {
+          return { ...res, readonly: false };
+        });
 
         entity.appendResults(results);
         ev.preventDefault();
       } catch (e) {
+        console.error(e);
         return;
       }
     };
@@ -46,7 +50,11 @@ export const useRegionsCopyPaste = (entity: any) => {
     const pasteHandler = (ev: Event) =>{
       const focusNode = window.getSelection()?.focusNode;
 
-      if (isFocusable(focusNode as HTMLElement) || isFocusable(document.activeElement)) return;
+      const nodeIsFocusable = isFocusable(focusNode as HTMLElement);
+      const activeElementIsFocusable = isFocusable(document.activeElement);
+
+      console.log('paste', { ev, nodeIsFocusable, activeElementIsFocusable });
+      if (nodeIsFocusable || activeElementIsFocusable) return;
 
       pasteFromClipboard(ev as ClipboardEvent);
     };
