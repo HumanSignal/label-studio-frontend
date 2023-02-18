9f3733860b3d6f64f093a2d546fab38fad23748f
hlomzik <hlomzik@gmail.com>
2023-02-18 03:31:18 +0000

    feat: DEV-4187: Select regions without 300ms lag (#1149)
    * feat: DEV-4187: Select regions without 300ms lag
    
    * Improve unselection speed as well for most configs

--- src/mixins/KonvaRegion.js
+++ src/mixins/KonvaRegion.js
@@ -38,21 +38,29 @@ export const KonvaRegionMixin = types.model({})
 
         if (e) e.cancelBubble = true;
 
+        const selectAction = () => {
+          self._selectArea(additiveMode);
+          deferredSelectId = null;
+        };
+
         if (annotation.editable && annotation.relationMode) {
           annotation.addRelation(self);
           annotation.stopRelationMode();
           annotation.regionStore.unselectAll();
         } else {
+          // Skip double click emulation when there is nothing to focus
+          if (!self.perRegionFocusTarget) {
+            selectAction();
+            return;
+          }
+          // Double click emulation
           if (deferredSelectId) {
             clearTimeout(deferredSelectId);
             self.requestPerRegionFocus();
             deferredSelectId = null;
             annotation.selectArea(self);
           } else {
-            deferredSelectId = setTimeout(() => {
-              self._selectArea(additiveMode);
-              deferredSelectId = null;
-            }, 300);
+            deferredSelectId = setTimeout(selectAction, 300);
           }
         }
       },
