1cb4c5449988a7f66a6497a3fdd1d353abe2c786
Sergey <sergey.koshevarov@heartex.com>
2023-02-20 23:04:17 +0000

    Remove dependencies from nyc config file

--- nyc.config.js
+++ nyc.config.js
@@ -1,7 +1,31 @@
 'use strict';
 
-const defaultExclude = require('@istanbuljs/schema/default-exclude');
-const defaultExtension = require('@istanbuljs/schema/default-extension');
+const defaultExtension = [
+  '.js',
+  '.cjs',
+  '.mjs',
+  '.ts',
+  '.tsx',
+  '.jsx',
+];
+const testFileExtensions = defaultExtension
+  .map(extension => extension.slice(1))
+  .join(',');
+const defaultExclude = [
+  'coverage/**',
+  'packages/*/test{,s}/**',
+  '**/*.d.ts',
+  'test{,s}/**',
+  `test{,-*}.{${testFileExtensions}}`,
+  `**/*{.,-}test.{${testFileExtensions}}`,
+  '**/__tests__/**',
+
+  /* Exclude common development tool configuration files */
+  '**/{ava,babel,nyc}.config.{js,cjs,mjs}',
+  '**/jest.config.{js,cjs,mjs,ts}',
+  '**/{karma,rollup,webpack}.config.js',
+  '**/.{eslint,mocha}rc.{js,cjs}',
+];
 
 module.exports = {
   include: ['src/**'],
