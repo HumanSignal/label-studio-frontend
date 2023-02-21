1f0e89e4a3abd349afe18338616c22795b30e4c5
hlomzik <hlomzik@gmail.com>
2023-02-21 16:46:40 +0000

    Simplify and fix Relations

--- src/components/RelationsOverlay/BoundingBox.js
+++ src/components/RelationsOverlay/BoundingBox.js
@@ -56,20 +56,6 @@ export class BoundingBox {
   }
 }
 
-const imageRelatedBBox = (region, bbox) => {
-  const imageBbox = Geometry.getDOMBBox(region.parent.stageRef.content, true);
-  const clampedBbox = Geometry.clampBBox(bbox,
-    { x:0, y:0 },
-    { x:region.parent.stageWidth, y:region.parent.stageHeight },
-  );
-
-  return {
-    ...clampedBbox,
-    x: imageBbox.x + clampedBbox.x,
-    y: imageBbox.y + clampedBbox.y,
-  };
-};
-
 const stageRelatedBBox = (region, bbox) => {
   // If there is no stageRef we just wait for it in the next renders
   if (!region.parent?.stageRef) return null;
@@ -110,40 +96,22 @@ const _detect = region => {
 
       return regionBbox;
     }
-    case 'rectangleregion': {
-      return stageRelatedBBox(
-        region,
-        Geometry.getRectBBox(region.x, region.y, region.width, region.height, region.rotation),
-      );
-    }
-    case 'ellipseregion': {
-      return stageRelatedBBox(
-        region,
-        Geometry.getEllipseBBox(region.x, region.y, region.radiusX, region.radiusY, region.rotation),
-      );
-    }
-    case 'polygonregion': {
-      return stageRelatedBBox(region, Geometry.getPolygonBBox(region.points));
-    }
-    case 'keypointregion': {
-      const imageBbox = Geometry.getDOMBBox(region.parent.imageRef, true);
-      const scale = region.parent.zoomScale;
-
-      return {
-        x: region.x * scale + imageBbox.x - 2,
-        y: region.y * scale + imageBbox.y - 2,
-        width: region.width,
-        height: region.width,
-      };
-    }
+    case 'rectangleregion':
+    case 'ellipseregion':
+    case 'polygonregion':
+    case 'keypointregion':
     case 'brushregion': {
-      // If there is no imageData we just wait for the next render
-      const bbox = region.imageData && Geometry.getImageDataBBox(region.imageData.data, region.imageData.width, region.imageData.height);
+      const bbox = region.bboxCoordsScreen;
 
-      return bbox && imageRelatedBBox(
+      return bbox ? stageRelatedBBox(
         region,
-        Geometry.getImageDataBBox(region.imageData.data, region.imageData.width, region.imageData.height),
-      );
+        {
+          x: bbox.left,
+          y: bbox.top,
+          width: bbox.right - bbox.left,
+          height: bbox.bottom - bbox.top,
+        },
+      ) : DEFAULT_BBOX;
     }
     default: {
       console.warn(`Unknown region type: ${region.type}`);
