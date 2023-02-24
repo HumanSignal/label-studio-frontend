4ad475f604fbafd896039dcdc58f78a870dff483
Julio Sgarbi <julio.sgarbi@hotmail.com>
2023-02-24 17:54:27 -0300

    add tests on text area lead_time

---
+++ e2e/tests/text-area.test.js
@@ -0,0 +1,41 @@
+
+/* global Feature, Scenario */
+
+const { serialize } = require('./helpers');
+const { fail } = require('assert');
+
+Feature('Text Area');
+
+const config = `
+<View> 
+  <Text name="text" size="10" value="$text"/> 
+  <TextArea name="ta" toName="text"></TextArea> 
+</View>
+`;
+
+const data = {
+  text: 'To have faith is to trust yourself to the water',
+};
+
+const params = { annotations: [{ id: 'test', result: [] }], config, data };
+
+Scenario('Check if text area is saving lead_time', async function({ I, LabelStudio, AtTextAreaView }) {
+  I.amOnPage('/');
+
+  LabelStudio.init(params);
+
+  AtTextAreaView.addNewTextTag('abcabc');
+
+  I.wait(1);
+
+  AtTextAreaView.addNewTextTag('abc abc abc abc');
+
+  I.wait(1);
+
+  AtTextAreaView.addNewTextTag('cba cba cba');
+
+  const result = await I.executeScript(serialize);
+
+  if (!result[0]?.meta?.lead_time || result[0].meta?.lead_time < 1)
+    fail('Lead time is not saved');
+});
