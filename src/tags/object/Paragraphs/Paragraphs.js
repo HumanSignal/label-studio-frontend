ce33005dfa9d3fc30751ec53fa40c7afff0ee339
hlomzik <hlomzik@gmail.com>
2023-03-16 00:52:11 +0000

    Add some comment about return type
    JSDoc can't parse tuple type, so let it be just `Array` with comment

--- src/tags/object/Paragraphs/Paragraphs.js
+++ src/tags/object/Paragraphs/Paragraphs.js
@@ -38,7 +38,7 @@ class HtxParagraphsView extends Component {
    * @param {HTMLElement} node
    * @param {number} offset
    * @param {boolean} [isStart=true]
-   * @return {Array}
+   * @return {Array} [offset, node, index, originalIndex]
    */
   getOffsetInPhraseElement(container, offset, isStart = true) {
     const node = this.getPhraseElement(container);
