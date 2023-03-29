51f269d80ee2b898510d403a9714c5666c08841d
Julio Sgarbi <julio.sgarbi@hotmail.com>
2023-03-29 19:26:01 -0300

    feat: LSDV-3025-1: LSDV-3025-2: Create reusable filter and filter row component

---
+++ src/components/Filter/FilterInput.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from 'react';
+// @ts-ignore
+import Input from '../../common/Input/Input';
+
+interface FilterInputInterface {
+  value: string | number | undefined;
+  type: string;
+  onChange: (value: any) => void;
+  placeholder?: string;
+  schema?: any;
+  style?: any;
+}
+
+export const FilterInput: FC<FilterInputInterface> = ({
+  value,
+  type,
+  onChange,
+  placeholder,
+  schema,
+  style,
+}) => {
+  const inputRef = React.useRef();
+  const onChangeHandler = () => {
+    const value = inputRef.current?.value ?? inputRef.current?.input?.value;
+
+    onChange(value);
+  };
+
+  return (
+    <Input
+      size='small'
+      type={type}
+      value={value ?? ''}
+      ref={inputRef}
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+      style={style}
+      {...(schema ?? {})}
+    />
+  );
+};
