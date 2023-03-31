e31c67e44e99e853a0333c3ce574b162c4b2668c
Nick Skriabin <nr@fenelon.ru>
2023-03-31 21:49:20 +0100

    Fix feature flags

--- __tests__/functional/cypress/support/e2e.ts
+++ __tests__/functional/cypress/support/e2e.ts
@@ -1,14 +1,12 @@
 import { CURRENT_FLAGS } from '../../feature-flags';
 
 before(() => {
-  cy.window().then(win => {
+  cy.on('window:before:load', (win) => {
     Object.assign(win, {
       APP_SETTINGS: {
         ...(win.APP_SETTINGS ?? {}),
         feature_flags: CURRENT_FLAGS,
       },
     });
-    cy.log('Default Feature Flags set');
-    cy.log(JSON.stringify(win.APP_SETTINGS.feature_flags, null, '  '));
   });
 });
