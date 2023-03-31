f0c69ebc6522b4083f7dcca27966523c2ebac82c
Nick Skriabin <nr@fenelon.ru>
2023-03-31 21:12:38 +0100

    Set default feature falgs before every test

--- __tests__/functional/feature-flags.ts
+++ __tests__/functional/feature-flags.ts
@@ -1,19 +1,19 @@
 import * as FLAGS from '../../src/utils/feature-flags';
 
+export const CURRENT_FLAGS = {
+  ...(window.APP_SETTINGS?.feature_flags ?? {}),
+  ...(window.__FEATURE_FLAGS__ ?? {}),
+  [FLAGS.FF_DEV_1170]: true,
+};
+
 Object.assign(window, {
-  APP_SETTINGS: {
-    ...(window.APP_SETTINGS ?? {}),
-    feature_flags: {
-      ...(window.APP_SETTINGS?.feature_flags ?? {}),
-      ...(window.__FEATURE_FLAGS__ ?? {}),
-      [FLAGS.FF_DEV_1170]: true,
-    },
-  },
+  ...(window.APP_SETTINGS ?? {}),
+  feature_flags: CURRENT_FLAGS,
 });
 
 function getFeatureFlags() {
   return {
-    ...(window.APP_SETTINGS?.feature_flags ?? {}),
+    ...(CURRENT_FLAGS ?? {}),
     // could be used to explicitly set flags for testing, i.e. [FF_DEV_3793]: true
   };
 }
