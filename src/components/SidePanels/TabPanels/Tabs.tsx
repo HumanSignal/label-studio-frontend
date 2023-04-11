eb9887e96b33927ee001001af6563fe3560dfca5
Travis1282 <travisjosephclark@gmail.com>
2023-04-10 22:25:00 -0500

    tabs content switching when in breakpoint lock mode

--- src/components/SidePanels/TabPanels/Tabs.tsx
+++ src/components/SidePanels/TabPanels/Tabs.tsx
@@ -181,8 +181,9 @@ const Tab = ({
 
 export const Tabs = (props: BaseProps) => {
 
-  const ActiveComponent = props.panelViews?.find(view => view.active)?.component;
-
+  const ActiveComponent = props.locked ? props.panelViews[props.breakPointActiveTab].component : props.panelViews?.find(view => view.active)?.component;
+  
+  console.log(props.panelViews[props.breakPointActiveTab]);
   return (
     <>
       <Block name="tabs">
