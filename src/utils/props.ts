6da345dec1d23216a154c42d050b203d36be8e6c
Sergey <sergey.koshevarov@heartex.com>
2023-04-13 15:56:41 +0100

    Add some comments about rotate interpolation

--- src/utils/props.ts
+++ src/utils/props.ts
@@ -20,8 +20,15 @@ export const interpolateProp = (start: SequenceItem, end: SequenceItem, frame: n
   // @todo edge cases
   const r = (frame - start.frame) / (end.frame - start.frame);
 
+  // Interpolation of angles is more tricky due to the cyclical nature of the angle value.
   if (prop === 'rotation') {
-    return normalizeAngle(start[prop] + normalizeAngle(end[prop] - start[prop]) * r);
+    // In order to perform interpolation over the shortest distance,
+    // we must normalize the difference in the values of the angles to the interval of [180;180) degrees,
+    // because this is analogous to [0;360) degrees,
+    // but at the same time the maximum absolute value remains the minimum possible 180 degrees.
+    const dAngle = normalizeAngle(end[prop] - start[prop]);
+
+    return normalizeAngle(start[prop] + dAngle * r);
   }
   return start[prop] + (end[prop] - start[prop]) * r;
 };
