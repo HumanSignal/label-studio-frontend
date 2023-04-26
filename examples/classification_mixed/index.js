b3db52b5184b166e922455c65a3297db8d16d204
hlomzik <hlomzik@gmail.com>
2023-04-26 19:47:34 +0100

    Fix and update ClassificationMixed example

--- examples/classification_mixed/index.js
+++ examples/classification_mixed/index.js
@@ -2,4 +2,4 @@ import config from './config.xml';
 import tasks from './tasks.json';
 import annotation from './annotations/1.json';
 
-export const ImageBbox = { config, tasks, annotation };
+export const ClassificationMixed = { config, tasks, annotation };
