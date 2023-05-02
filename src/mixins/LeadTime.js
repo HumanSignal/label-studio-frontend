afb16462bfa39cc8b7f211e2bec24ea5e38b5f74
Travis1282 <travisjosephclark@gmail.com>
2023-05-02 11:16:44 -0500

    rename debounce interval

--- src/mixins/LeadTime.js
+++ src/mixins/LeadTime.js
@@ -1,7 +1,7 @@
 import { types } from 'mobx-state-tree';
 import debounce from  'lodash.debounce';
 
-const DEBOUNCE_INTERVAL = 500;
+const LEAD_TIME_CALCULATION_INTERVAL = 500;
 
 /**
  * Calculate lead time just by calling `countTime()`. Result stored in `leadTime`.
@@ -17,13 +17,13 @@ const LeadTimeMixin = types
     internalCountTime(callTime) {
       const now = +Date.now();
       // leading call called immediately, debounced call called after INTERVAL
-      const leadingCall = now - callTime < DEBOUNCE_INTERVAL / 2;
+      const leadingCall = now - callTime < LEAD_TIME_CALCULATION_INTERVAL / 2;
 
       if (leadingCall) {
         // debounced fn called only once if event is rare, so if this is leading call,
         // but there is previous call, assume that previous event took INTERVAL time
         if (self.startTime) {
-          self.leadTime += DEBOUNCE_INTERVAL;
+          self.leadTime += LEAD_TIME_CALCULATION_INTERVAL;
         }
         self.startTime = now;
       } else {
@@ -34,12 +34,12 @@ const LeadTimeMixin = types
       }
     },
 
-    debouncedCountTime: debounce(time => self.internalCountTime(time), DEBOUNCE_INTERVAL, { leading: true }),
+    debouncedCountTime: debounce(time => self.internalCountTime(time), LEAD_TIME_CALCULATION_INTERVAL, { leading: true }),
 
     // for manual calls with distance in time it's neccessary to emulate second debounced call,
     // so fake the time of call; trailingCall can be an event, so check explicitely for `true`
     countTime(trailingCall = false) {
-      const emulateDebounced = trailingCall === true ? -DEBOUNCE_INTERVAL : 0;
+      const emulateDebounced = trailingCall === true ? -LEAD_TIME_CALCULATION_INTERVAL : 0;
 
       self.debouncedCountTime(+new Date() + emulateDebounced);
     },
