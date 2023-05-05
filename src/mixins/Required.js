279d5a489e54343dd9ba131b431512ecf800fc3c
hlomzik <hlomzik@gmail.com>
2023-05-05 08:49:54 +0100

    Fix Required — validate only if required=true

--- src/mixins/Required.js
+++ src/mixins/Required.js
@@ -7,6 +7,8 @@ const RequiredMixin = types
   })
   .actions(self => ({
     validate() {
+      if (!self.required) return true;
+
       if (self.perregion) {
         // validating when choices labeling is done per region,
         // for example choice may be required to be selected for
