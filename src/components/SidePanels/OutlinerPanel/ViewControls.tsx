4ec89df9f9ae7df51b92a4cfdb7fb64a92b1efba
Julio Sgarbi <julio.sgarbi@hotmail.com>
2023-05-10 17:11:29 -0300

    fix: LSDV-5074: Add a FF to outliner filter

--- src/components/SidePanels/OutlinerPanel/ViewControls.tsx
+++ src/components/SidePanels/OutlinerPanel/ViewControls.tsx
@@ -9,7 +9,7 @@ import { BemWithSpecifiContext } from '../../../utils/bem';
 import { SidePanelsContext } from '../SidePanelsContext';
 import './ViewControls.styl';
 import { Filter } from '../../Filter/Filter';
-import { FF_DEV_3873, isFF } from '../../../utils/feature-flags';
+import { FF_DEV_3873, FF_LSDV_3025, isFF } from '../../../utils/feature-flags';
 
 const { Block, Elem } = BemWithSpecifiContext();
 
@@ -108,7 +108,7 @@ export const ViewControls: FC<ViewControlsProps> = ({
           )}
         </Elem>
       )}
-      {isFF(FF_DEV_3873) && (
+      {isFF(FF_LSDV_3025) && (
         <Filter
           onChange={onFilterChange}
           filterData={regions?.regions}
