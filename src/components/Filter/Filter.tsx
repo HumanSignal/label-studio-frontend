9f5b3764764b9f0769a447680c68da6fd5964ce2
Travis Clark <travisjosephclark@gmail.com>
2023-05-10 08:35:18 -0500

    fix: LSDV-5031: Adding new region after filter it is not filtered (#1352)
    * fix: lsdv-5031: if filter contains all regions it should be null
    
    * filter list applied to new additions
    
    ---------
    
    Co-authored-by: Travis1282 <Travis1282@users.noreply.github.com>

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -20,7 +20,7 @@ export const Filter: FC<FilterInterface> = ({
 
   useEffect(() => {
     if(filterList.length > 0) {
-      onChange(FilterItems(filterData, filterList[0]));
+      onChange(FilterItems(filterData, filterList));
     }
   }, [filterData]);
 
