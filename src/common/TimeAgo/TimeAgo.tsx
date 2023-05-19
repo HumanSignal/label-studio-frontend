9daf77debcd19008283405981d744d629811a696
bmartel <brandonmartel@gmail.com>
2023-05-19 09:23:18 -0500

    Update src/common/TimeAgo/TimeAgo.tsx

--- src/common/TimeAgo/TimeAgo.tsx
+++ src/common/TimeAgo/TimeAgo.tsx
@@ -51,7 +51,10 @@ export const TimeAgo = ({ date, ...rest }: TimeAgoProps) => {
     };
   }, [date, timestamp]);
 
+  // Replace the longer english text when less than a minute in time. This is done this way due to a limiting API
+  // with the date-fns function. If we require an entire overhaul to the messaging for the en-US locale, revisit this and replace with an entire locale override option.
   const text = formatDistanceToNow(fromTS, { addSuffix: true }) === 'less than a minute ago' ? 'seconds ago' : formatDistanceToNow(fromTS, { addSuffix: true });
+
 
   return (
     <time
