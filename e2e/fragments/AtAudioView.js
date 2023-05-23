801d8b2038b3e6c67959dde3dc6f663e9bb7b99b
Nick Skriabin <nr@fenelon.ru>
2023-05-23 14:24:58 +0100

    Fix matching

--- e2e/fragments/AtAudioView.js
+++ e2e/fragments/AtAudioView.js
@@ -278,7 +278,7 @@ module.exports = {
     selector = selector ? this[selector] : this._errorSelector;
     const error = await I.grabTextFrom(selector);
 
-    assert.match(new RegExp(error, 'g'), value);
+    assert.match(error, new RegExp(value, 'g'));
   },
   
   /**
