1b4aabdc48815d4c076bb5def285b920a8819de4
Travis1282 <travisjosephclark@gmail.com>
2023-05-25 18:34:01 -0500

    uhnoist unfreese command on page resize

--- src/tags/object/Image/Image.js
+++ src/tags/object/Image/Image.js
@@ -946,12 +946,11 @@ const Model = types.model({
       });
       self.drawingRegion?.updateImageSize(width / naturalWidth, height / naturalHeight, width, height, userResize);
 
-      setTimeout(self.annotation.history.unfreeze, 0);
-
+      self.annotation.history.unfreeze();
       //sometimes when user zoomed in, annotation was creating a new history. This fix that in case the user has nothing in the history yet
       if (_historyLength <= 1) {
         // Don't force unselection of regions during the updateObjects callback from history reinit
-        setTimeout(() => self.annotation.reinitHistory(false), 0);
+        self.annotation.reinitHistory(false);
       }
     },
 
