fb21e6cdb577dade39258dfd7b3aecae188ae206
bmartel <brandonmartel@gmail.com>
2023-05-30 17:22:30 -0500

    chore: 1.8.0 cherrypick 2023 05 30 (#1422)
    * fix: LSDV-5161: Fix confidence score design to match with new labeling ui (#1408)
    
    * fix: LSDV-5161: Fix confidence score layout to match with new labeling ui
    
    * add tooltip
    
    * fix: LSDV-5165: Number indices in List data (#1405)
    
    Allow numeric indices in List items; this will also unblock Preview.
    Also show List title, it was missed in the code.
    
    * fix: LSDV-5167: Ranker undo/redo (#1416)
    
    * fix: LSDV-5167: Fix undo/redo for Ranker
    
    Store original List in results as well to track all changes
    to every column including the original one.
    This will result in this column exposed to exported result,
    but that should not be a problem.
    
    * Add note about _ column
    
    * Handle readonly status (i.e. from predictions)
    
    * Small fixes
    
    Add data-ranker-id to every item.
    Fix types for html param.
    
    * Trigger view reaction in rank mode with array
    
    ---------
    
    Co-authored-by: hlomzik <hlomzik@users.noreply.github.com>
    
    * fix: LSDV-5155: Panel Hover color, button style improvements  (#1409)
    
    * uhnoist unfreese command on page resize
    
    * fix: lsdv-5155: panel header hover state, button improvements
    
    * undo image tag changes commited by mistake
    
    * undo image tag changes commited by mistake
    
    * remove color from svg icons
    
    * Update collapse-small.svg
    
    * Update expand-small.svg
    
    * Update arrow-right-small.svg
    
    * Update arrow-left-small.svg
    
    * remove color from svg icons
    
    ---------
    
    Co-authored-by: bmartel <brandonmartel@gmail.com>
    
    * fix: LSDV-5198: Fix the annotation name line height (#1417)
    
    * fix: LSDV-5198: Fix the annotation name line height
    
    * fix: LSDV-5194: Add annotation tab indicator tooltip
    
    * rollback changes
    
    * add height property on the user name
    
    ---------
    
    Co-authored-by: juliosgarbi <juliosgarbi@users.noreply.github.com>
    
    * fix: LSDV-5194: Add annotation tab indicator tooltip (#1418)
    
    * fix: LSDV-5194: Add annotation tab indicator tooltip
    
    * fix comment icons, and inconsistencies in file names, as well as annotation carousel nav button states
    
    * removing changes accidentally committed for annotation nav buttons
    
    ---------
    
    Co-authored-by: Brandon Martel <brandonmartel@gmail.com>
    Co-authored-by: juliosgarbi <juliosgarbi@users.noreply.github.com>
    
    * fix: LSDV-5153: Change icon sort (#1410)
    
    * fix: LSDV-5153: Change icon sort
    
    * remove leftover caracter
    
    * fix button and style of direction menu
    
    * remove unused css
    
    * removing deps from get ordering label
    
    * fix extra name and padding
    
    * removing unused imports
    
    ---------
    
    Co-authored-by: juliosgarbi <juliosgarbi@users.noreply.github.com>
    
    * fix: LSDV-5145: Regions hover lock consistency (#1411)
    
    * fix: lsdv-5145: regions hover lock consistancy
    
    * fix the hover states for outliner item controls in new ui
    
    ---------
    
    Co-authored-by: Brandon Martel <brandonmartel@gmail.com>
    
    ---------
    
    Co-authored-by: Julio Sgarbi <julio.sgarbi@hotmail.com>
    Co-authored-by: hlomzik <hlomzik@gmail.com>
    Co-authored-by: hlomzik <hlomzik@users.noreply.github.com>
    Co-authored-by: Travis Clark <travisjosephclark@gmail.com>
    Co-authored-by: juliosgarbi <juliosgarbi@users.noreply.github.com>

--- src/tags/object/List.js
+++ src/tags/object/List.js
@@ -59,7 +59,7 @@ const Model = types
     get dataSource() {
       return {
         items: self.items,
-        columns: [{ id: self.name }],
+        columns: [{ id: self.name, title: self.title }],
         itemIds: { [self.name]: Object.keys(self.items) },
       };
     },
@@ -73,7 +73,8 @@ const Model = types
 
       if (!Array.isArray(value)) return;
 
-      self._value = value;
+      // ids should be strings
+      self._value = value.map(item => ({ ...item, id: String(item.id) }));
     },
   }));
 
