e7edfaf0f6f63fe61039eeaed42f3dd1bd8811a6
Julio Sgarbi <julio.sgarbi@hotmail.com>
2023-06-19 13:41:52 -0300

    fix: LSDV-4853: Add warning message when user selects more choices than maxUsages

--- tests/functional/specs/control_tags/taxonomy.cy.ts
+++ tests/functional/specs/control_tags/taxonomy.cy.ts
@@ -1,5 +1,10 @@
 import { LabelStudio, Taxonomy, Tooltip } from '@heartexlabs/ls-test/helpers/LSF/index';
-import { simpleData, taxonomyConfig } from '../../data/control_tags/taxonomy';
+import {
+  dataWithPrediction,
+  simpleData,
+  taxonomyConfig,
+  taxonomyConfigWithMaxUsages
+} from '../../data/control_tags/taxonomy';
 
 describe('Control Tags - Taxonomy', () => {
   it('should show hint for <Choice />', () => {
@@ -12,5 +17,33 @@ describe('Control Tags - Taxonomy', () => {
     Taxonomy.open();
     Taxonomy.findItem('Choice 2').trigger('mouseenter');
     Tooltip.hasText('A hint for Choice 2');
+  });
+
+  it('should show error message if there is more choices selected than maxUsages is set', () => {
+    LabelStudio.params()
+      .config(taxonomyConfigWithMaxUsages)
+      .data(dataWithPrediction)
+      .withResult([{
+        'id': 'n2ldmNpSQI',
+        'type': 'taxonomy',
+        'value': {
+          'taxonomy': [
+            [
+              'Archaea',
+            ],
+            [
+              'Bacteria',
+            ],
+          ],
+        },
+        'origin': 'manual',
+        'to_name': 'text',
+        'from_name': 'taxonomy',
+      }])
+      .init();
+
+    cy.contains('button', 'Update').click();
+
+    cy.contains('The number of options selected (2) exceed the maximum allowed (1). To proceed, first unselect excess options for: • Taxonomy (taxonomy)').should('exist');
   });
 });
