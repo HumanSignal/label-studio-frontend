628745b97ed64f5384ecd836c349160045049a3f
hlomzik <hlomzik@gmail.com>
2023-06-20 19:04:13 +0100

    Add test for preselected Taxonomy; with 3617 FF off

--- tests/functional/data/control_tags/taxonomy.ts
+++ tests/functional/data/control_tags/taxonomy.ts
@@ -3,9 +3,9 @@ export const simpleData = {
 };
 export const taxonomyConfig = `<View>
   <Text name="text"/>
-  <Taxonomy name="choices">
+  <Taxonomy name="choices" toName="text">
     <Choice value="Choice 1" />
     <Choice value="Choice 2" hint="A hint for Choice 2" />
-    <Choice value="Choice 3" />
+    <Choice value="Choice 3" selected="true" />
   </Taxonomy>
 </View>`;
--- tests/functional/specs/control_tags/taxonomy.cy.ts
+++ tests/functional/specs/control_tags/taxonomy.cy.ts
@@ -1,5 +1,12 @@
 import { LabelStudio, Taxonomy, Tooltip } from '@heartexlabs/ls-test/helpers/LSF/index';
 import { simpleData, taxonomyConfig } from '../../data/control_tags/taxonomy';
+import { FF_DEV_2100_A, FF_DEV_3617 } from '../../../../src/utils/feature-flags';
+
+beforeEach(() => {
+  LabelStudio.addFeatureFlagsOnPageLoad({
+    [FF_DEV_2100_A]: true, // preselected choices
+  });
+});
 
 describe('Control Tags - Taxonomy', () => {
   it('should show hint for <Choice />', () => {
@@ -12,5 +19,41 @@ describe('Control Tags - Taxonomy', () => {
     Taxonomy.open();
     Taxonomy.findItem('Choice 2').trigger('mouseenter');
     Tooltip.hasText('A hint for Choice 2');
+    Taxonomy.findItem('Choice 3').find('[type=checkbox]').should('not.be.checked');
+  });
+});
+
+const init = () => {
+  LabelStudio.params()
+    .config(taxonomyConfig)
+    .data(simpleData)
+    .withResult([])
+    .init();
+};
+
+describe('Control Tags - Taxonomy with preselected Choices', () => {
+  // @todo fix this test
+  xit('should work with FF_DEV_3617 on', () => {
+    LabelStudio.addFeatureFlagsOnPageLoad({
+      [FF_DEV_3617]: true,
+    });
+
+    init();
+    cy.get('.lsf-annotations-list').click();
+    cy.get('.lsf-annotations-list__create').click();
+    Taxonomy.open();
+    Taxonomy.findItem('Choice 3').find('[type=checkbox]').should('be.checked');
+  });
+
+  it('should work with FF_DEV_3617 off', () => {
+    LabelStudio.addFeatureFlagsOnPageLoad({
+      [FF_DEV_3617]: false,
+    });
+
+    init();
+    cy.get('.lsf-annotations-list').click();
+    cy.get('.lsf-annotations-list__create').click();
+    Taxonomy.open();
+    Taxonomy.findItem('Choice 3').find('[type=checkbox]').should('be.checked');
   });
 });
