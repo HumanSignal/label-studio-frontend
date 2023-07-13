a8859e584ddc8cd85855f7ad2a35a1aa6e454335
Julio Sgarbi <julio.sgarbi@hotmail.com>
2023-07-13 10:36:12 -0300

    round the the seconds

--- src/tags/object/Paragraphs/Phrases.js
+++ src/tags/object/Paragraphs/Phrases.js
@@ -11,7 +11,7 @@ const formatTime = (seconds) => {
 
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
-  const remainingSeconds = seconds % 60;
+  const remainingSeconds = Math.round(seconds % 60);
 
   const formattedHours = String(hours).padStart(2, '0');
   const formattedMinutes = String(minutes).padStart(2, '0');
