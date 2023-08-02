e3fb679b4c96029fe8914cb26bc628e6c1b9c67b
Sergey <sergey.koshevarov@heartex.com>
2023-08-02 21:07:14 +0100

    fix: LSDV-5446: Prevent cleaning svg tags

--- src/utils/reactCleaner.js
+++ src/utils/reactCleaner.js
@@ -1,5 +1,3 @@
-const svgChildren = new WeakMap();
-
 export function cutFibers(object) {
   const objects = [object];
   let obj;
@@ -7,28 +5,23 @@ export function cutFibers(object) {
   while ((obj = objects.pop())) {
     const keys = Object.keys(obj);
     const descriptors = Object.getOwnPropertyDescriptors(obj);
+    const isSvg = obj.elementType === 'svg';
+
+    // preventing processing svs due to the problem with props,
+    // props sometimes come from the global variables, so it's tricky to clean them without breaking icons itself
+    if (isSvg) continue;
 
     for (const key of keys) {
       const prop = obj[key];
       const isWritable = descriptors[key].writable;
-      const isSvgRelated = obj.elementType === 'svg' || svgChildren.has(obj);
 
-      if (isSvgRelated) {
-        console.log('!> SvgRelated', obj);
-      }
       if (prop && isWritable) {
         if (key !== '_debugOwner' && typeof prop === 'object' && {}.hasOwnProperty.call(prop, 'stateNode')) {
           objects.push(obj[key]);
-          if (isSvgRelated) {
-            svgChildren.set(obj[key], true);
-          }
         }
         if (typeof prop === 'object' || typeof prop === 'function') {
           obj[key] = null;
         }
-      }
-      if (isSvgRelated) {
-        svgChildren.delete(obj);
       }
     }
   }
@@ -50,6 +43,9 @@ export function findReactKey(node) {
 export function cleanDomAfterReact(nodes, reactKey) {
   for (const node of nodes) {
     if (node.isConnected) return;
+    // preventing processing svs due to the problem with props,
+    // props sometimes come from the global variables, so it's tricky to clean them without breaking icons itself
+    if (node.tagName === 'svg') return;
     const reactPropKeys = (Object.keys(node)).filter(key => key.startsWith('__react') && (!RegExp(/^(?:__reactProps|__reactFiber)/).exec(key) || RegExp(new RegExp(`\\${reactKey}$`)).exec(key)));
 
     if (reactPropKeys.length) {
