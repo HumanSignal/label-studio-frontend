da5875bcbe755d001e7ce8e2498037dab06ae407
Sergey <sergey.koshevarov@heartex.com>
2023-08-31 14:42:23 +0100

    Remove unused import

--- src/regions/BrushRegion.js
+++ src/regions/BrushRegion.js
@@ -18,7 +18,7 @@ import IsReadyMixin from '../mixins/IsReadyMixin';
 import { KonvaRegionMixin } from '../mixins/KonvaRegion';
 import { ImageModel } from '../tags/object/Image';
 import { colorToRGBAArray, rgbArrayToHex } from '../utils/colors';
-import { FF_DBLCLICK_DELAY, FF_DEV_3793, FF_DEV_4081, isFF } from '../utils/feature-flags';
+import { FF_DEV_3793, FF_DEV_4081, isFF } from '../utils/feature-flags';
 import { AliveRegion } from './AliveRegion';
 import { RegionWrapper } from './RegionWrapper';
 
