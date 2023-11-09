9c86b89433c3bc075a454f48d9fc8d54fea7c4d5
Max Tkachenko <makseq@gmail.com>
2023-11-09 23:57:02 +0000

    docs: Add paragraphs example with audio  (#927)
    Update Model.js
    
    Co-authored-by: hlomzik <hlomzik@gmail.com>
    Co-authored-by: hlomzik <hlomzik@users.noreply.github.com>

--- src/tags/object/Paragraphs/model.js
+++ src/tags/object/Paragraphs/model.js
@@ -30,6 +30,29 @@ import styles from './Paragraphs.module.scss';
  *     <Label value="Random talk"></Label>
  *   </ParagraphLabels>
  * </View>
+ * @example
+ * <!-- Paragraphs with audio -->
+ * <View>
+ *   <Paragraphs audioUrl="$audio" value="$para" name="paragraphs"
+ *               layout="dialogue" textKey="text" nameKey="author"
+ *               showPlayer="true"
+ *               />
+ *   
+ *   <Choices name="choices" toName="paragraphs" choice="multiple">
+ *       <Choice value="Good quality"/>
+ *       <Choice value="Fast speech"/>
+ *   </Choices>    
+ * </View>
+ * 
+ * <!-- {"data": { 
+ *   "para": [
+ *     {"text": "test 1", "author": "A", "start": 0.0, "end": 1.0},
+ *     {"text": "test 2", "author": "B", "start": 1.0, "end": 2.0},
+ *     {"text": "test 3", "author": "A", "start": 2.0, "end": 3.0}
+ *   ], 
+ *   "audio": "/static/samples/game.wav"
+ * }}
+ * -->
  * @name Paragraphs
  * @regions ParagraphsRegion
  * @meta_title Paragraph Tags for Paragraphs
