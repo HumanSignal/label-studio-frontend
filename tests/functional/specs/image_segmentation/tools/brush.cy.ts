0d3f90dd16e8c225771e6c1d08dcc5e4ba1f1d1b
Sergey <gondragos@gmail.com>
2023-11-27 15:59:52 +0000

    Add tests

---
+++ tests/functional/specs/image_segmentation/tools/brush.cy.ts
@@ -0,0 +1,99 @@
+import { ImageView, Labels, LabelStudio, Sidebar } from '../../../../../../../ls-frontend-test/helpers/LSF';
+import { brushCenteredConfig, brushConfig, brushImageData } from '../../../data/image_segmentation/tools/brush';
+import { Generator } from '../../../../../../../ls-frontend-test/helpers/common/Generator';
+
+describe('Brush tool', () => {
+  it('Should be able to draw over the same brush region', () => {
+    LabelStudio.params()
+      .config(brushConfig)
+      .data(brushImageData)
+      .withResult([])
+      .init();
+
+    ImageView.waitForImage();
+
+    cy.log('Draw the region');
+    Labels.select('Label B');
+    ImageView.drawPolylineRelative([[0.1, 0.1], [0.9, 0.1], [0.1, 0.12], [0.9, 0.12], [0.1, 0.14], [0.9, 0.14]]);
+
+    cy.log('Continue drawing over the first region');
+    ImageView.drawPolylineRelative([[0.1, 0.11], [0.9, 0.11], [0.1, 0.13], [0.9, 0.13]]);
+
+    cy.log('The region, the label and the tool sholdn\'t be unselected during the drawing');
+    cy.log('Repeat once again');
+    ImageView.drawPolylineRelative([[0.3, 0.12], [0.7, 0.12]]);
+
+    cy.log('As well as we drawing the same region, there should be only one region at the sidebar');
+    Sidebar.hasRegions(1);
+  });
+
+  it('Should be able to erase continuously', () => {
+    LabelStudio.params()
+      .config(brushConfig)
+      .data(brushImageData)
+      .withResult([])
+      .init();
+
+    ImageView.waitForImage();
+
+    cy.log('Draw the region');
+    Labels.select('Label B');
+    ImageView.drawPolylineRelative([[0.1, 0.1], [0.9, 0.1], [0.1, 0.12], [0.9, 0.12], [0.1, 0.14], [0.9, 0.14]]);
+
+    ImageView.selectEraserToolByButton();
+    ImageView.drawPolylineRelative([[0.3, 0.12], [0.7, 0.12]]);
+
+    Sidebar.hasSelectedRegion(0);
+  });
+
+  it('Should draw only within the boundaries of the canvas', () => {
+    Generator.generateImageUrl({ width: 50, height: 100 })
+      .then((imageData) => {
+        LabelStudio.params()
+          .config(brushConfig)
+          .data({ image: imageData })
+          .withResult([])
+          .init();
+
+        ImageView.waitForImage();
+
+        Labels.select('Label A');
+        ImageView.drawPolylineRelative([[0.1, 0.3], [2, 0.7]], {
+          beforeMouseup() {
+            ImageView.pixelRelativeShouldBe(0.1, 0.3, '#64b164');
+            ImageView.pixelRelativeShouldBe(2, 0.7, '#fafafa');
+          },
+        });
+
+        ImageView.pixelRelativeShouldBe(0.1, 0.3, '#64b164');
+        ImageView.pixelRelativeShouldBe(2, 0.7, '#fafafa');
+      });
+  });
+
+  it('Should draw only within the boundaries of the canvas even with centered image', () => {
+    Generator.generateImageUrl({ width: 50, height: 100 })
+      .then((imageData) => {
+        LabelStudio.params()
+          .config(brushCenteredConfig)
+          .data({ image: imageData })
+          .withResult([])
+          .init();
+
+        ImageView.waitForImage();
+
+        Labels.select('Label A');
+        ImageView.drawingArea.trigger('mousemove', 353, 282);
+        ImageView.drawPolylineRelative([[0.5, 0.3], [2, 0.7]], {
+          beforeMouseup() {
+            ImageView.pixelRelativeShouldBe(0.5, 0.3, '#fafafa');
+            ImageView.pixelRelativeShouldBe(1, 0.433, '#64b164');
+            ImageView.pixelRelativeShouldBe(2, 0.7, '#fafafa');
+          },
+        });
+
+        ImageView.pixelRelativeShouldBe(0.5, 0.3, '#fafafa');
+        ImageView.pixelRelativeShouldBe(1, 0.433, '#64b164');
+        ImageView.pixelRelativeShouldBe(2, 0.7, '#fafafa');
+      });
+  });
+});
