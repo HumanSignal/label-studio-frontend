afb0887a8bc4e2a246dff13c01c6296e74ebf84a
Sergei Ivashchenko <triklozoid@gmail.com>
2023-11-30 00:02:00 +0000

    fix: LEAP-419: Fix brush issues appeared after fixing zoom performance (#1630)
    Co-authored-by: Sergey <gondragos@gmail.com>

---
+++ tests/functional/data/image_segmentation/tools/magic-wand.ts
@@ -0,0 +1,10 @@
+export const magicWandConfig = `
+<View>
+  <Image name="image" value="$image" crossOrigin="anonymous" />
+  <MagicWand name="magicwand" toName="image" />
+</View>
+`;
+
+export const magicWandImageData = {
+  image: 'https://htx-pub.s3.amazonaws.com/samples/magicwand/magic_wand_scale_1_20200902_015806_26_2235_1B_AnalyticMS_00750_00750.jpg',
+};
