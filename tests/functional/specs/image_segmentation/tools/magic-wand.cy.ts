a3e4f43f823890cbc40f712634defbff003124dd
Sergey <sergey.koshevarov@heartex.com>
2024-01-09 21:10:34 +0000

    fix: LEAP-439: Add maskData exporting into new MIG serialization approach (#1659)
    * fix: LEAP-439: Add maskData exporting into new MIG serialization approach
    
    * Update version of @heartexlabs/ls-test dependency
    
    * Add serialization/deserialization test for Magic Wand tool
    
    * fix import path
    
    * Configure timeouts
    
    * Update @heartexlabs/ls-test dependency version
    
    * Update @heartexlabs/ls-test dependency
    
    * Update "@heartexlabs/ls-test" dependency

--- tests/functional/specs/image_segmentation/tools/magic-wand.cy.ts
+++ tests/functional/specs/image_segmentation/tools/magic-wand.cy.ts
@@ -1,6 +1,7 @@
 import { ImageView, LabelStudio, Sidebar } from '@heartexlabs/ls-test/helpers/LSF';
 import { magicWandConfig, magicWandImageData } from '../../../data/image_segmentation/tools/magic-wand';
-import { FF_DEV_4081 } from '../../../../../src/utils/feature-flags';
+import { FF_DEV_4081, FF_LSDV_4583 } from '../../../../../src/utils/feature-flags';
+import { Generator } from '@heartexlabs/ls-test/helpers/common/Generator';
 
 describe('Magic Wand tool', () => {
   beforeEach(() => {
@@ -39,5 +40,42 @@ describe('Magic Wand tool', () => {
     ImageView.clickAtRelative(.5, .5);
 
     Sidebar.hasRegions(1);
+  });
+
+  it.only('Should be able to serialize and deserialize the same region', () => {
+    LabelStudio.addFeatureFlagsOnPageLoad({
+      [FF_LSDV_4583]: true,
+    });
+    Generator.generateImageUrl({ width: 10, height: 10 })
+      .then((imageUrl) => {
+        LabelStudio.params()
+          .config(magicWandConfig)
+          .data({ image: imageUrl })
+          .withResult([])
+          .init();
+
+        ImageView.waitForImage();
+
+        ImageView.clickAtRelative(.15, .15);
+        ImageView.waitForPixelRelative(.15, .15, '#a1a1a1', { timeout: 10000 });
+
+        Sidebar.hasRegions(1);
+        ImageView.capture('RegionOnCanvas');
+
+        LabelStudio.serialize().then((data) => {
+          LabelStudio.params()
+            .config(magicWandConfig)
+            .data({ image: imageUrl })
+            .withResult(data)
+            .init();
+
+          ImageView.waitForImage();
+
+          Sidebar.hasRegions(1);
+          ImageView.waitForPixelRelative(.15, .15, '#a1a1a1', { timeout: 10000 });
+          ImageView.canvasShouldNotChange('RegionOnCanvas');
+        });
+      });
+
   });
 });
