927da2d73587bda24d2074547a8945631ec3bed2
Sergey <gondragos@gmail.com>
2024-01-23 16:43:09 +0000

    Add tests for checking labels behaviour

--- tests/functional/specs/image_segmentation/tools/rect.cy.ts
+++ tests/functional/specs/image_segmentation/tools/rect.cy.ts
@@ -1,4 +1,4 @@
-import { ImageView, LabelStudio, Sidebar } from '@heartexlabs/ls-test/helpers/LSF';
+import { ImageView, Labels, LabelStudio, Sidebar } from '@heartexlabs/ls-test/helpers/LSF';
 import {
   rectangleToolAndLabelsConfig,
   simpleImageData,
@@ -22,10 +22,34 @@ describe('Rectangle tool', () => {
 
     Sidebar.hasRegions(1);
     Sidebar.toggleRegionSelection(0);
+    Labels.select('Label 1');
 
     ImageView.clickAtRelative(0.5, 0.5);
     ImageView.clickAtRelative(0.7, 0.7);
     Sidebar.hasRegions(1);
+    Labels.selectedLabel.should('have.length', 0);
+  });
+
+  it('should draw rectangle by dragging (FF_DEV_1442 = true)', () => {
+    LabelStudio.addFeatureFlagsOnPageLoad({
+      [FF_DEV_1442]: true,
+    });
+
+    LabelStudio.params()
+      .config(rectangleToolAndLabelsConfig)
+      .data(simpleImageData)
+      .withResult(simpleRectangleResult)
+      .init();
+
+    LabelStudio.waitForObjectsReady();
+
+    Sidebar.hasRegions(1);
+    Sidebar.toggleRegionSelection(0);
+    Labels.select('Label 1');
+
+    ImageView.drawRectRelative(0.5, 0.5, 0.2, 0.2);
+    Sidebar.hasRegions(2);
+    Labels.selectedLabel.should('contain.text', 'Label 1');
   });
 
   it('should draw rectangle when clicking outside (FF_DEV_1442 = false)', () => {
@@ -43,9 +67,33 @@ describe('Rectangle tool', () => {
 
     Sidebar.hasRegions(1);
     Sidebar.toggleRegionSelection(0);
+    Labels.select('Label 1');
 
     ImageView.clickAtRelative(0.5, 0.5);
     ImageView.clickAtRelative(0.7, 0.7);
     Sidebar.hasRegions(2);
+    Labels.selectedLabel.should('contain.text', 'Label 1');
+  });
+
+  it('should draw rectangle by dragging (FF_DEV_1442 = false)', () => {
+    LabelStudio.addFeatureFlagsOnPageLoad({
+      [FF_DEV_1442]: false,
+    });
+
+    LabelStudio.params()
+      .config(rectangleToolAndLabelsConfig)
+      .data(simpleImageData)
+      .withResult(simpleRectangleResult)
+      .init();
+
+    LabelStudio.waitForObjectsReady();
+
+    Sidebar.hasRegions(1);
+    Sidebar.toggleRegionSelection(0);
+    Labels.select('Label 1');
+
+    ImageView.drawRectRelative(0.5, 0.5, 0.2, 0.2);
+    Sidebar.hasRegions(2);
+    Labels.selectedLabel.should('contain.text', 'Label 1');
   });
 });
