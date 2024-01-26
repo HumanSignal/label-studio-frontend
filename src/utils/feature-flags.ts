b06334e880f0f09e3beeb8799f78ed9f08d4a121
hlomzik <hlomzik@gmail.com>
2024-01-26 17:33:22 +0000

    Add a description for this FF

--- src/utils/feature-flags.ts
+++ src/utils/feature-flags.ts
@@ -325,6 +325,7 @@ export const FF_TAXONOMY_SELECTED = 'fflag_fix_front_leap_149_taxonomy_selected_
 /** Skip video to region start when it's selected in the outliner */
 export const FF_LEAP_187 = 'fflag_feat_front_leap_187_video_seek_on_select_short';
 
+/** Select annotation only once during store init and trigger the rest from this select */
 export const FF_SIMPLE_INIT = 'fflag_fix_front_leap_443_select_annotation_once';
 
 /**
